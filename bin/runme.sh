dd5223f9b2173eb53f4e2f6abb358d9fba7e204c
Toralf Förster <toralf.foerster@gmx.de>
2016-03-29 22:40:38 +0200

    runme.sh: keep exit code, add mail
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/runme.sh
+++ bin/runme.sh
@@ -4,30 +4,45 @@
 
 # wrapper to allow us to change job.sh whilst a copy of it is in use
 #
+mailto="tinderbox@zwiebeltoralf.de"
 
 # these are paths within a chroot image
 #
 orig=/tmp/tb/bin/job.sh
 copy=/tmp/job.sh
 
+rc=-1
+
 while :;
 do
   # 2 checks to avoid a race during copy operation
   #
-  cp $orig $copy || exit $?
+  cp $orig $copy
+  rc=$?
+  if [[ $rc -ne 0 ]]; then
+    break
+  fi
+
   if [[ -s $copy ]]; then
     /bin/bash -n $copy
+    rc=$?
 
-    if [[ $? -eq 0 ]]; then
+    if [[ $rc -eq 0 ]]; then
       /bin/bash $copy
-      # rc=125: job.sh detected a newer version of itself
+      rc=$?
+
+      # rc=125: job.sh detected a newer version of
       #
-      if [[ $? -eq 125 ]]; then
-        continue
+      if [[ $rc -ne 125 ]]; then
+        break
       fi
-      break
     fi
   fi
 done
 
-exit $?
+if [[ $rc -ne 0 ]]; then
+  name=$(grep "^PORTAGE_ELOG_MAILFROM=" /etc/portage/make.conf | cut -f2 -d '"' | cut -f1 -d ' ')
+  date | mail -s "$(date) $name rc=$rc" $mailto
+fi
+
+exit $rc
