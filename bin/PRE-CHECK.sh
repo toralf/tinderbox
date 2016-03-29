6f285d490a1ac5cb7321f16b8a1c3c7ca4965c23
Toralf Förster <toralf.foerster@gmx.de>
2016-03-29 22:39:38 +0200

    PRE-CHECK.sh: remove the symlink check
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/PRE-CHECK.sh
+++ bin/PRE-CHECK.sh
@@ -8,16 +8,6 @@
 
 rc=0
 
-# is resolv.conf symlinked to /var/run ?
-# https://bugs.gentoo.org/show_bug.cgi?id=555694
-#
-if [[ -L /etc/resolv.conf ]]; then
-  ls -l /etc/resolv.conf
-  rm /etc/resolv.conf && cp /etc/resolv.conf.bak /etc/resolv.conf
-
-  rc=1
-fi
-
 # misplaced/left files
 #
 for i in /%{__unitdir} /installed_by_webapp_eclass /tmp/nb-scan-cach /tmp/build.*.tmp.c*
