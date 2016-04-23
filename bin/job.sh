3bf529bd9ecc7e1cfc7ab4c4a6e07cbb7211471d
Toralf Förster <toralf.foerster@gmx.de>
2016-04-23 21:25:30 +0200

    bin/job.sh: prefix versions with their keyword
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/job.sh
+++ bin/job.sh
@@ -290,7 +290,7 @@ emerge --info >> $issuedir/emerge-info.txt
   cp $issuedir/issue $issuedir/body
   cat << EOF >> $issuedir/body
 
-versions: $(eshowkw -a amd64 $currShort | grep -A 100 '^-' | grep -v '^-' | awk ' { print $1 } ' | xargs)
+versions: $(eshowkw -a amd64 $currShort | grep -A 100 '^-' | grep -v '^-' | awk '{ if ($3 == "+") { print $1 } else { print $3$1 } }' | xargs)
 assignee: $(cat $issuedir/assignee)
 cc:       $(cat $issuedir/cc)
 https://bugs.gentoo.org/buglist.cgi?query_format=advanced&resolution=---&short_desc=$currShort&short_desc_type=allwordssubstr
