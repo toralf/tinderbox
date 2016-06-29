755afcb518e0b86abea8b05b5c91d22d0734a33e
Toralf Förster <toralf.foerster@gmx.de>
2016-06-29 17:38:47 +0200

    bin/switch2libressl.sh: fix typo in emerging openssh
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/switch2libressl.sh
+++ bin/switch2libressl.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #
-#set -x
+# set -x
 
-echo 'USE="${USE} libressl -openssl"' >> /etc/portage/make.conf
+sed -i -e 's/ [+-]*openssl/[ ]*/g' -e 's/ [+-]*libressl[ ]*/ /' /etc/portage/make.conf
+sed -i -e 's/USE="/USE="-openssl libressl/'                     /etc/portage/make.conf
 
 mkdir -p /etc/portage/profile
-echo "-libressl"                      >> /etc/portage/profile/use.stable.mask
+echo "-libressl"          >> /etc/portage/profile/use.stable.mask
 
-echo "dev-libs/openssl"               >> /etc/portage/package.mask/openssl
-echo "dev-libs/libressl"              >> /etc/portage/package.accept_keywords/libressl
+echo "dev-libs/openssl"   >> /etc/portage/package.mask/openssl
+echo "dev-libs/libressl"  >> /etc/portage/package.accept_keywords/libressl
 
 echo "=dev-lang/python-2.7.11-r2"           >> /etc/portage/package.accept_keywords/libressl
 echo "=dev-lang/python-3.4.3-r7"            >> /etc/portage/package.accept_keywords/libressl
@@ -22,7 +23,7 @@ echo "dev-lang/erlang"                      >> /etc/portage/package.accept_keywo
 emerge -f libressl  &&\
 emerge -C openssl   &&\
 emerge -1 libressl  &&\
-emerge -1 openss    &&\
+emerge -1 openssh   &&\
 emerge -1 wget      &&\
 
 emerge -1 =dev-lang/python-2.7.11-r2 =dev-lang/python-3.4.3-r7  &&\
