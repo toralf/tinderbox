e32a5ad36bb78fd4944e850fa2dea2cb622abcff
Toralf Förster <toralf.foerster@gmx.de>
2016-06-29 23:58:02 +0200

    bin/switch2libressl.sh: fix CURL_SSL
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/switch2libressl.sh
+++ bin/switch2libressl.sh
@@ -2,24 +2,25 @@
 #
 # set -x
 
-sed -i -e 's/ [+-]*openssl/[ ]*/g' -e 's/ [+-]*libressl[ ]*/ /' /etc/portage/make.conf
-sed -i -e 's/USE="/USE="-openssl libressl/'                     /etc/portage/make.conf
-echo '"CURL_SSL="-openssl libressl"'                         >> /etc/portage/make.conf
+sed -i -e 's/ [+-]*openssl/[ ]*/g' -e 's/ [+-]*libressl[ ]*/ /'       /etc/portage/make.conf
+sed -i -e 's/USE="/CURL_SSL="libressl"\nUSE="-openssl libressl \n  /' /etc/portage/make.conf
 
 mkdir -p /etc/portage/profile
-echo "-libressl"          >> /etc/portage/profile/use.stable.mask
+echo "-libressl"          > /etc/portage/profile/use.stable.mask
 
-echo "dev-libs/openssl"   >> /etc/portage/package.mask/openssl
-echo "dev-libs/libressl"  >> /etc/portage/package.accept_keywords/libressl
+echo "dev-libs/openssl"   > /etc/portage/package.mask/openssl
+echo "dev-libs/libressl"  > /etc/portage/package.accept_keywords/libressl
 
-echo "=dev-lang/python-2.7.11-r2"           >> /etc/portage/package.accept_keywords/libressl
-echo "=dev-lang/python-3.4.3-r7"            >> /etc/portage/package.accept_keywords/libressl
-echo "=app-eselect/eselect-python-20160222" >> /etc/portage/package.accept_keywords/libressl
-echo "=dev-lang/python-exec-2.4.3"          >> /etc/portage/package.accept_keywords/libressl
-echo "=net-misc/iputils-20121221-r2"        >> /etc/portage/package.accept_keywords/libressl
+cat << EOF > /etc/portage/package.accept_keywords/libressl
+=dev-lang/python-2.7.11-r2
+=dev-lang/python-3.4.3-r7
+=app-eselect/eselect-python-20160222
+=dev-lang/python-exec-2.4.3
+=net-misc/iputils-20121221-r2
 
-echo "dev-libs/libevent"                    >> /etc/portage/package.accept_keywords/libressl
-echo "dev-lang/erlang"                      >> /etc/portage/package.accept_keywords/libressl
+dev-libs/libevent
+dev-lang/erlang
+EOF
 
 emerge -f libressl  &&\
 emerge -C openssl   &&\
