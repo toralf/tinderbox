9d064dc5df91337b69a8fd64cbd4a92d29e7e534
Toralf Förster <toralf.foerster@gmx.de>
2016-08-31 13:52:04 +0200

    bin/switch2libressl.sh: python2|3 subversions are subjects to change
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/switch2libressl.sh
+++ bin/switch2libressl.sh
@@ -35,9 +35,12 @@ echo "-libressl"          > /etc/portage/profile/use.stable.mask
 echo "dev-libs/openssl"   > /etc/portage/package.mask/openssl
 echo "dev-libs/libressl"  > /etc/portage/package.accept_keywords/libressl
 
+py2="dev-lang/python-2.7.11-r2"
+py3="dev-lang/python-3.4.3-r7"
+
 cat << EOF > /etc/portage/package.accept_keywords/libressl
-=dev-lang/python-2.7.11-r2
-=dev-lang/python-3.4.3-r7
+=$py2
+=$py3
 =app-eselect/eselect-python-20160222
 =dev-lang/python-exec-2.4.3
 =net-misc/iputils-20121221-r2
@@ -52,8 +55,8 @@ emerge -1 libressl  &&\
 emerge -1 openssh   &&\
 emerge -1 wget      &&\
 
-emerge -1 =dev-lang/python-2.7.11-r2 =dev-lang/python-3.4.3-r7  &&\
-emerge -1 =net-misc/iputils-20121221-r2                         &&\
+emerge -1 =$py2 =$py3                           &&\
+emerge -1 =net-misc/iputils-20121221-r2         &&\
 emerge @preserved-rebuild
 
 exit $?
