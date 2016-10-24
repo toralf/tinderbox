d9df5c7375d377e98041a4017d86879b1008213a
Toralf Förster <toralf.foerster@gmx.de>
2016-10-24 10:20:34 +0200

    bin/tbs.sh: reduce likelihiood of gcc-6, ffmpeg-3 and ABI=32
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/tbs.sh
+++ bin/tbs.sh
@@ -242,15 +242,15 @@ EOF
   chmod a+rw  etc/portage/package.mask/self
 
   if [[ "$keyword" = "unstable" ]]; then
-    # unmask ffmpeg-3 at 50% of unstable images
+    # unmask ffmpeg-3 at 25% of unstable images
     #
-    if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+    if [[ $(($RANDOM % 4)) -eq 0 ]]; then
       echo "media-video/ffmpeg" > etc/portage/package.unmask/ffmpeg
     fi
 
-    # unmask GCC-6 at 50% of unstable images
+    # unmask GCC-6 at 25% of unstable images
     #
-    if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+    if [[ $(($RANDOM % 4)) -eq 0 ]]; then
       echo "sys-devel/gcc:6.2.0"    > etc/portage/package.unmask/gcc-6
       echo "sys-devel/gcc:6.2.0 **" > etc/portage/package.accept_keywords/gcc-6
     fi
@@ -537,9 +537,9 @@ if [[ $(($RANDOM % 4)) -eq 0 ]]; then
 else
   libressl="n"
 fi
-# 50% ABI_X86="32 64"
+# 25% ABI_X86="32 64"
 #
-if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+if [[ $(($RANDOM % 4)) -eq 0 ]]; then
   multilib="y"
 else
   multilib="n"
