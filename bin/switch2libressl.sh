30337b060135b56fc8d60197d55dfa84a3f5842c
Toralf Förster <toralf.foerster@gmx.de>
2017-01-11 10:30:07 +0100

    bin/switch2libressl.sh: 2 py packages do not need keywording any longer
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/switch2libressl.sh
+++ bin/switch2libressl.sh
@@ -60,9 +60,6 @@ if [[ $? -ne 0 ]]; then
 dev-libs/libressl
 $py2
 $py3
-~app-eselect/eselect-python-20160516
-~dev-lang/python-exec-2.4.3
-
 ~dev-libs/libevent-2.1.5
 ~mail-mta/ssmtp-2.64
 ~net-nds/openldap-2.4.44
