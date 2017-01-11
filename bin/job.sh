085e4cf473cfca803e8b1f1a3e4a5c10b1c8953e
Toralf Förster <toralf.foerster@gmx.de>
2017-01-11 18:37:19 +0100

    bin/job.sh: factor out AppendMeOntoIssue()
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/job.sh
+++ bin/job.sh
@@ -221,6 +221,19 @@ function GetMailAddresses() {
 }
 
 
+function AppendThisIsMeToIssue() {
+  cat << EOF >> $issuedir/issue
+
+  -----------------------------------------------------------------
+
+  This is an $keyword amd64 chroot image (named $name) at a hardened host acting as a tinderbox.
+
+  -----------------------------------------------------------------
+
+EOF
+}
+
+
 # helper of GotAnIssue()
 # create an email containing convenient links and command lines ready for copy+paste
 #
@@ -367,13 +380,9 @@ cc:       $(cat $issuedir/cc)
 
 EOF
 
+  AppendThisIsMeToIssue
+
   cat << EOF >> $issuedir/issue
-
-  -----------------------------------------------------------------
-
-  This is an $keyword amd64 chroot image (named $name) at a hardened host acting as a tinderbox.
-
-  -----------------------------------------------------------------
 gcc-config -l:
 $(gcc-config -l 2>&1                && echo)
 llvm-config --version:
@@ -881,6 +890,7 @@ function ParseElogForQA() {
       chmod 777 $issuedir
       echo "$failed : installs into paths that should be created at runtime" > $issuedir/title
       cp $i $issuedir/issue
+      AppendThisIsMeToIssue
       GetMailAddresses
       id=$(bugz -q --columns 400 search --show-status $short "installs into paths" | sort -u -n | tail -n 1 | tee $issuedir/body | cut -f1 -d ' ')
       echo -e "\n~/tb/bin/bgo.sh -d ~/run/$name/$issuedir -b 520404 -s QA\n" >> $issuedir/body
