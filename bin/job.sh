dd5e9c87b2c3a9a6d55136273b660597d9a35348
Toralf Förster <toralf.foerster@gmx.de>
2017-02-11 16:56:36 +0100

    bin/job.sh: shorten stresc() drastically
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/job.sh
+++ bin/job.sh
@@ -14,7 +14,7 @@
 # strip away escape sequences
 #
 function stresc() {
-  perl -MTerm::ANSIColor=colorstrip -nle 'colorstrip; s/\e\[K//g; s/\e\[\[//g; s/\e\[\(B//g; s/\r/\n/g; s/\x00/<NULL>/g; s/\e\[m//g; s/\e\[.m//g; s/\e\[..m//g; s/..\(B.\[m//g; print'
+  perl -wne 's/\x1b\x28\x42//g; s/\r/\n/g; s/\x00/<NULL>/g; print'
 }
 
 
