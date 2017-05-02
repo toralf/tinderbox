b1b284d588a25bab7678f27b139f391e17779518
Toralf Förster <toralf.foerster@gmx.de>
2017-05-02 20:25:57 +0200

    bin/logwatch.sh: start once, run forever
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/logwatch.sh
+++ bin/logwatch.sh
@@ -4,19 +4,22 @@
 
 mailto="tinderbox@zwiebeltoralf.de"
 
-if [[ -s ~/nohup.out ]]; then
-  (ls -l ~/nohup.out; head -n 500 ~/nohup.out) | timeout 120 mail -s "nohup.out is non-empty" $mailto
-  truncate -s 0 ~/nohup.out
-fi
-
 f=/tmp/tinderbox.logwatch.out
 
-if [[ ! -f $f ]]; then
-  if [[ "$(wc -c ~/logs/* 2>/dev/null | tail -n 1)" != "0 total" ]]; then
-    ls -l ~/logs/* >> $f
-    head ~/logs/*  >> $f
-    echo -e "\nto re-activate this test again, do:  rm $f" >> $f
-    cat $f | timeout 120 mail -s "logs are non-empty" $mailto
-  fi
-fi
+while :; do
+  if [[ ! -f $f ]]; then
+    if [[ -s ~/nohup.out ]]; then
+      (ls -l ~/nohup.out; head -n 500 ~/nohup.out) | mail -s "nohup.out is non-empty" $mailto
+      truncate -s 0 ~/nohup.out
+    fi
 
+    if [[ "$(wc -c ~/logs/* 2>/dev/null | tail -n 1)" != "0 total" ]]; then
+      ls -l ~/logs/* >> $f
+      head ~/logs/*  >> $f
+      echo -e "\nto re-activate this test again, do:  rm $f" >> $f
+
+      cat $f | mail -s "logs are non-empty" $mailto
+    fi
+  fi
+  sleep 60
+done
