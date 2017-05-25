5a7cd0edcff0f1c3317d86e8a3be8f92fb40e5a0
Toralf Förster <toralf.foerster@gmx.de>
2017-05-25 09:02:06 +0200

    bin/job.sh: prettify perl one-liner
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/job.sh
+++ bin/job.sh
@@ -11,7 +11,14 @@
 # hint: colorstrip() doesn't modify its argument, instead it returns the result
 #
 function stresc() {
-  perl -MTerm::ANSIColor=colorstrip -nle '$_ = colorstrip($_); s,\r,\n,g; s/\x00/<0x00>/g; s/\x1b\x28\x42//g; s/\x1b\x5b\x4b//g; print'
+  perl -MTerm::ANSIColor=colorstrip -nle '
+    $_ = colorstrip($_);
+    s,\r,\n,g;
+    s/\x00/<0x00>/g;
+    s/\x1b\x28\x42//g;
+    s/\x1b\x5b\x4b//g;
+    print;
+  '
 }
 
 
