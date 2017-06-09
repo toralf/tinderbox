8567b3af1ad418c16a869476228625934f444ea9
Toralf Förster <toralf.foerster@gmx.de>
2017-06-09 23:37:55 +0200

    bin/job.sh: gather all *.log files in work dir
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/job.sh
+++ bin/job.sh
@@ -224,7 +224,7 @@ EOF
     #
     f=/tmp/files
     rm -f $f
-    (cd "$workdir" && find ./ -name "config.log" > $f && [[ -s $f ]] && tar -cjpf $issuedir/files/config.log.tbz2 $(cat $f) && rm $f)
+    (cd "$workdir" && find ./ -name "*.log" > $f && [[ -s $f ]] && tar -cjpf $issuedir/files/logs.tbz2 $(cat $f) && rm $f)
 
     # provide the whole temp dir if it exists
     #
