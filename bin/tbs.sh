fe707886673a0a27ac25dde85919cddce1e742b8
Toralf Förster <toralf.foerster@gmx.de>
2017-06-11 09:38:39 +0200

    bin/tbs.sh: restrict to a subset of default/linux/amd64/ profiles
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/tbs.sh
+++ bin/tbs.sh
@@ -561,7 +561,7 @@ suffix=""       # will be appended onto the name before the timestamp
 
 # set defaults for profile, keyword, ssl vendor and ABI_X86
 #
-profile=$(eselect profile list | awk ' { print $2 } ' | grep -e "^default" | grep -v -e '/x32' -e "/developer" -e '/musl' -e "/uclibc" -e "/selinux" | sort --random-sort | head -n1)
+profile=$(eselect profile list | awk ' { print $2 } ' | grep -e "^default/linux/amd64" | cut -f4- -d'/' | grep -v -e 'x32' -e 'developer' | sort --random-sort | head -n1)
 
 keyword="unstable"
 
