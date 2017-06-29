02353055eb0745919bad9016aa50f146e7f08b3c
Toralf Förster <toralf.foerster@gmx.de>
2017-06-29 22:33:11 +0200

    bin/logcheck.sh: fix cornercase of just 1 file after reboot
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/logcheck.sh
+++ bin/logcheck.sh
@@ -13,7 +13,7 @@ while :; do
       truncate -s 0 ~/nohup.out
     fi
 
-    if [[ -n "$(ls -l ~/logs/* 2>/dev/null)" && "$(wc -c ~/logs/* 2>/dev/null | tail -n 1)" != "0 total" ]]; then
+    if [[ -n "$(ls -l ~/logs/)" && "$(wc -c ~/logs/* 2>/dev/null | tail -n 1 | cut -f1 -d' ')" != "0" ]]; then
       ls -l ~/logs/* >> $f
       head ~/logs/*  >> $f
       echo -e "\nto re-activate this test again, do:  rm $f" >> $f
