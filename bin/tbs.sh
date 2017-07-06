6ca6f3d4e1d47f95759a7feb67a5bcbb46476e21
Toralf Förster <toralf.foerster@gmx.de>
2017-07-06 21:14:43 +0200

    bin/tbs.sh: fix a cut -m-s fallout typo
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/tbs.sh
+++ bin/tbs.sh
@@ -18,11 +18,15 @@
 function rufs()  {
   (
     grep -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use.desc
-    grep -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use.local.desc | cut -f2 -d ':' m -s
+    grep -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use.local.desc | cut -f2 -d ':' -s
   ) |\
   cut -f1 -d ' ' |\
-  grep -v -e 'hostname' -e 'linguas' -e 'make-symlinks' -e 'musl' -e 'pax' -e 'qt4' -e 'selinux' -e 'ssl' -e 'static' -e 'systemd' -e 'test' -e 'tls' -e 'uclibc' |\
-  sort -u -R | head -n $(($RANDOM % 99)) | sort |\
+  grep -v   -e 'build' -e 'hostname' -e 'linguas' -e 'make-symlinks' -e 'musl'  \
+            -e 'pax' -e 'qt4' -e 'selinux' -e 'ssl' -e 'static' -e 'systemd'    \
+            -e 'test' -e 'tls' -e 'uclibc' |\
+  sort -u -R |\
+  head -n $(($RANDOM % 99)) |\
+  sort |\
   while read flag
   do
     if [[ $(($RANDOM % 5)) -eq 0 ]]; then
