564cab686fa86136a5e4099d7f86c3bb44d7aca1
Toralf Förster <toralf.foerster@gmx.de>
2017-08-30 22:27:03 +0200

    bin/insert_pkgs.sh: 3 hours are default, overwrite it with $1
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/insert_pkgs.sh
+++ bin/insert_pkgs.sh
@@ -55,7 +55,7 @@ acmr=$(mktemp /tmp/acmrXXXXXX)
 # add 1 hour for the mirrors to be in sync with their masters
 #
 cd /usr/portage/
-git diff --diff-filter=ACMR --name-status "@{ 3 hour ago }".."@{ 1 hour ago }" 2>/dev/null |\
+git diff --diff-filter=ACMR --name-status "@{ ${1:-3} hour ago }".."@{ 1 hour ago }" 2>/dev/null |\
 grep -F -e '/files/' -e '.ebuild' -e '/Manifest' | cut -f2- -s | xargs -n 1 | cut -f1-2 -d'/' -s | sort --unique > $acmr
 
 info="# $(basename $0) at $(date): $(wc -l < $acmr) ACMR packages"
