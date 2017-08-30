ce1833f8225e2e33d9ec157426f55559a5363f76
Toralf Förster <toralf.foerster@gmx.de>
2017-08-30 21:52:29 +0200

    bin/whatsup.sh: grep for STOP im $pks too
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/whatsup.sh
+++ bin/whatsup.sh
@@ -53,8 +53,9 @@ function Overall() {
     fi
     todo=$(wc -l 2>/dev/null < $i/tmp/packages)
     ((todo=todo+0))
-    [[ -f $i/tmp/LOCK ]] && lck="y" || lck=""
-    [[ -f $i/tmp/STOP ]] && stp="y" || stp=""
+    [[ -f $i/tmp/LOCK ]] && lck="L" || lck=""
+    [[ -f $i/tmp/STOP ]] && stp="S" || stp=""
+    grep -q "^STOP" $i/tmp/packages && stp="${stp}s"
     d=$(basename $(dirname $i))
     b=$(basename $i)
     [[ -e ~/run/$b ]] && d="run"
