427577a6409313cb9465274cc370a83c2c49b196
Toralf Förster <toralf.foerster@gmx.de>
2017-09-02 20:46:12 +0200

    bin/pre-check.sh: check for bug 629602
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/pre-check.sh
+++ bin/pre-check.sh
@@ -2,33 +2,31 @@
 #
 #set -x
 
-# this script checks for artefacts left by the last task
+# this script checks for artefacts/issues of the last task
 #
-
-# bug       pattern
-#
-# 623336    /tmp/tttest.*
-
-exit 0
-
-# helper to prevent duplicate reports
-#
-findings=/tmp/$(basename $0).list
-if [[ ! -f $findings ]]; then
-  touch $findings
-fi
 
 rc=0
-for i in /tmp/tttest.*
-do
-  if [[ -e $i ]]; then
-    grep -e "^${i}$" $findings
-    if [[ $? -eq 1 ]]; then
-      ls -ld $i
-      echo "$i" >> $findings
-      rc=1
-    fi
-  fi
-done
+
+# findings=/tmp/$(basename $0).list
+# if [[ ! -f $findings ]]; then
+#   touch $findings
+# fi
+#
+# for i in
+# do
+#   if [[ -e $i ]]; then
+#     grep -e "^${i}$" $findings
+#     if [[ $? -eq 1 ]]; then
+#       ls -ld $i
+#       echo "$i" >> $findings
+#       rc=1
+#     fi
+#   fi
+# done
+
+if [[ "$task" =~ "dbus" &&  ! -f /etc/machine-id ]]; then
+  echo "/etc/machine-id is missing"
+  rc=1
+fi
 
 exit $rc
