00bbd805622e12406ec2ef8074891753b0316a14
Toralf Förster <toralf.foerster@gmx.de>
2017-09-29 20:08:24 +0200

    bin/bgo.sh: there is no _emerge* file for QA issues
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/bgo.sh
+++ bin/bgo.sh
@@ -152,7 +152,7 @@ fi
 
 # test failures aren't show stopper
 #
-bzgrep -q " \* ERROR:.* failed (.* phase):" $dir/_emerge_*
+bzgrep -q " \* ERROR:.* failed (.* phase):" $dir/_emerge_* 2>/dev/null
 if [[ $? -eq 0 ]]; then
   bugz modify --set-keywords TESTFAILURE $id 1>bugz.out 2>bugz.err || Warn $?
 fi
