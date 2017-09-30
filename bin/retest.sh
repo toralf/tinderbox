6ee1c09b151d6e29758a560cec41f7e9a4c1925d
Toralf Förster <toralf.foerster@gmx.de>
2017-09-30 21:58:36 +0200

    bin/retest.sh: do not use stdin
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/retest.sh
+++ bin/retest.sh
@@ -2,7 +2,7 @@
 #
 # set -x
 
-# read from stdin package(s) and re-test them
+# retest package(s)
 #
 
 if [[ ! "$(whoami)" = "tinderbox" ]]; then
@@ -10,24 +10,24 @@ if [[ ! "$(whoami)" = "tinderbox" ]]; then
   exit 1
 fi
 
-xargs -n1 |\
+echo ${@} |\
 while read line
 do
-  # split away the version/revision
+  # split away the version/revision if applicable
   #
   p=$(qatom "$line" | sed 's/[ ]*(null)[ ]*//g' | cut -f1-2 -d' ' -s | tr ' ' '/')
   if [[ -z "$p" ]]; then
     p=$line
   fi
 
-  # remove all package entries made by job.sh
+  # remove all image specific package exceptions
   #
   sed -i -e "/$(echo $p | sed -e 's,/,\\/,')/d"  \
     ~/tb/data/ALREADY_CATCHED                   \
     ~/run/*/etc/portage/package.mask/self       \
     ~/run/*/etc/portage/package.env/{cxx,nosandbox,notest} 2>/dev/null
 
-  # don't put this into backlog.upd b/c it could be delayed by a huge repository update
+  # don't use backlog.upd b/c that content is shuffled around by update_backlog.sh
   #
   for i in ~/run/*
   do
