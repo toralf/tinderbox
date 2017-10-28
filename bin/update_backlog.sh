06a77e212ed0d298b8f91c182fd16b078b09de2e
Toralf Förster <toralf.foerster@gmx.de>
2017-10-28 23:06:41 +0200

    bin/update_backlog.sh: ignore a changed Manifest file
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/update_backlog.sh
+++ bin/update_backlog.sh
@@ -12,22 +12,26 @@ if [[ ! "$(whoami)" = "tinderbox" ]]; then
   exit 1
 fi
 
-# holds the package names of added/changed/modified/renamed ebuilds
+# list of added/changed/modified/renamed ebuilds
 #
 acmr=/tmp/$(basename $0).acmr
 
+cd /usr/portage/
 # add 1 hour to let mirrors be in sync
 #
-cd /usr/portage/
 git diff --diff-filter=ACMR --name-status "@{ ${1:-2} hour ago }".."@{ 1 hour ago }" 2>/dev/null |\
-grep -F -e '/files/' -e '.ebuild' -e '/Manifest' | cut -f2- -s | xargs -n 1 | cut -f1-2 -d'/' -s | sort --unique |\
+#
+# ./files might contain (new) patches
+#
+grep -F -e '/files/' -e '.ebuild' | cut -f2- -s | xargs -n 1 | cut -f1-2 -d'/' -s | sort --unique |\
 grep -v -f ~/tb/data/IGNORE_PACKAGES > $acmr
 
 if [[ -s $acmr ]]; then
   for i in ~/run/*
   do
     bl=$i/tmp/backlog.upd
-    # re-shuffle to avoid emerging the same package at different images at the same time
+    # mix with current backlog to avoid
+    # emerging a package in many images at the same time
     #
     sort -u --random-sort $bl $acmr > $bl.tmp && cp $bl.tmp $bl
     rm $bl.tmp
