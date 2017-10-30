b633b6026abdebdca9372a508c97fb419da05696
Toralf Förster <toralf.foerster@gmx.de>
2017-10-30 20:56:51 +0100

    bin/setup_img.sh: prepare new env "override"
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -311,6 +311,10 @@ EOF
   #
   echo 'FEATURES="-test"'                   > ./etc/portage/env/notest
 
+  # special (perl) tests: https://bugs.gentoo.org/635516
+  #
+  echo 'DIST_TEST_OVERRIDE="do verbose"'    > ./etc/portage/env/override
+
   # breakage is forced in job.sh by the XDG_* variables
   #
   echo 'FEATURES="-sandbox -usersandbox"'   > ./etc/portage/env/nosandbox
