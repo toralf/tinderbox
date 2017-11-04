86a030705889c292571426298b420d273cec71fd
Toralf Förster <toralf.foerster@gmx.de>
2017-11-04 16:48:26 +0100

    bin/retest.sh: use backlog.upd instad backlog
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/retest.sh
+++ bin/retest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-set -x
+# set -x
 
 # retest package(s)
 #
@@ -10,34 +10,30 @@ if [[ ! "$(whoami)" = "tinderbox" ]]; then
   exit 1
 fi
 
-echo $* |\
-xargs -n 1 |\
+# split away version/revision if possible
+#
+echo $* | xargs -n 1 |\
 while read line
 do
   if [[ -z "$line" ]]; then
     continue
   fi
 
-  # split away the version/revision if applicable
-  #
   p=$(qatom "$line" | sed 's/[ ]*(null)[ ]*//g' | cut -f1-2 -d' ' -s | tr ' ' '/')
   if [[ -z "$p" ]]; then
     p=$line
   fi
 
-  # remove all image specific package exceptions
-  #
   sed -i -e "/$(echo $p | sed -e 's,/,\\/,')/d"  \
     ~/tb/data/ALREADY_CATCHED                   \
     ~/run/*/etc/portage/package.mask/self       \
     ~/run/*/etc/portage/package.env/{cxx,nosandbox,notest} 2>/dev/null
 
-  # backlog.upd is the 2nd best choice b/c that is shuffled around by update_backlog.sh
-  #
   for i in ~/run/*
   do
-    echo "$p" >> $i/tmp/backlog
+    echo "$p" >> $i/tmp/backlog.upd
   done
+
 done
 
 exit 0
