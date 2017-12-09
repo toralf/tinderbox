7c6d18fed7c3efd89b53e1414d16c89457b16875
Toralf Förster <toralf.foerster@gmx.de>
2017-12-09 16:57:52 +0100

    bin/setup_img.sh: prettify ThrowUseFlags()
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -13,30 +13,30 @@
 # functions
 #
 
+# set *up to* n-1 arbitrarily choosen USE flags in make.conf, about 1/m of them being masked
+#
 function ThrowUseFlags()  {
-  # grab up to n-1 USE flags, mask about 1/m of them
-  #
   n=75
   m=10
 
   (
-    grep -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use.desc
-    grep -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use.local.desc | cut -f2 -d ':' -s
+    grep -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use.desc                            | cut -f1 -d ' '
+    grep -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use.local.desc | cut -f2 -d ':' -s  | cut -f1 -d ' '
   ) |\
-  cut -f1 -d ' ' |\
-  grep -v   -e '32' -e '64' -e "^armv" -e 'bindist' -e 'build' -e 'cdinstall' \
-            -e 'gcj' -e 'hostname' -e 'kill' -e 'linguas' -e 'make-symlinks' -e 'minimal' -e 'multilib' -e 'musl'  \
-            -e 'oci8' -e 'pax' -e 'pic' -e 'qt4' -e 'tools' -e 'selinux' -e 'ssl' -e 'ssp' -e 'static' -e 'systemd'    \
-            -e 'test' -e 'tls' -e 'uclibc' -e 'valgrind' -e 'vim-syntax' |\
+  grep -v -e '32' -e '64' -e "^armv" -e 'bindist' -e 'build' -e 'cdinstall' \
+          -e 'gcj' -e 'hostname' -e 'kill' -e 'linguas' -e 'make-symlinks' -e 'minimal' -e 'multilib' -e 'musl'  \
+          -e 'oci8' -e 'pax' -e 'pic' -e 'qt4' -e 'tools' -e 'selinux' -e 'ssl' -e 'ssp' -e 'static' -e 'systemd'    \
+          -e 'test' -e 'tls' -e 'uclibc' -e 'valgrind' -e 'vim-syntax' |\
   sort -u --random-sort |\
   head -n $(($RANDOM % $n)) |\
   sort |\
   while read flag
   do
     if [[ $(($RANDOM % $m)) -eq 0 ]]; then
-      echo -n "-"
+      echo -n "-$flag "
+    else
+      echo -n "$flag "
     fi
-    echo -n "$flag "
   done
 }
 
