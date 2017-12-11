d02fc98f9c9ae903538271ab23dcaedef4f89774
Toralf Förster <toralf.foerster@gmx.de>
2017-12-11 19:31:46 +0100

    bin/setup_img.sh: use current mirrorselect results
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -404,7 +404,7 @@ PORTAGE_ELOG_MAILFROM="$name <tinderbox@localhost>"
 PORTAGE_GPG_DIR="/var/lib/gentoo/gkeys/keyrings/gentoo/release"
 PORTAGE_GPG_KEY="F45B2CE82473685B6F6DCAAD23217DA79B888F45"
 
-GENTOO_MIRRORS="$wgethost rsync://mirror.netcologne.de/gentoo/ ftp://sunsite.informatik.rwth-aachen.de/pub/Linux/gor.bytemark.co.uk/gentoo/ rsync://ftp.snt.utwente.nl/gentoo"
+GENTOO_MIRRORS="http://mirror.netcologne.de/gentoo/ http://ftp.halifax.rwth-aachen.de/gentoo/ http://ftp.uni-erlangen.de/pub/mirrors/gentoo http://ftp-stud.hs-esslingen.de/pub/Mirrors/gentoo/"
 
 # https://bugs.gentoo.org/640290
 #
