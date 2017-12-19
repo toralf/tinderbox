390fab293f165c56c68d0f9e18aa68b7a1d31262
Toralf Förster <toralf.foerster@gmx.de>
2017-12-19 21:15:29 +0100

    bin/chr.sh: factor out CGroup logic, comments
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/chr.sh
+++ bin/chr.sh
@@ -8,31 +8,37 @@
 #
 # $> sudo /opt/tb/bin/chr.sh ~/run/plasma-unstable_20150811-144142
 
-# if a mount fails then bail out immediately
+# mount the directories shared by the host
 #
 function mountall() {
+  # if a mount fails then bail out immediately
+  #
 
   # system dirs
   #
   /bin/mount -t proc       proc        $mnt/proc   &&\
   /bin/mount --rbind       /sys        $mnt/sys    &&\
-  /bin/mount --make-rslave $mnt/sys                &&\
+  /bin/mount --make-rslave             $mnt/sys    &&\
   /bin/mount --rbind       /dev        $mnt/dev    &&\
-  /bin/mount --make-rslave $mnt/dev                &&\
-  # portage and tinderbox
+  /bin/mount --make-rslave             $mnt/dev    &&\
   #
-  /bin/mount -o bind       /home/tinderbox/tb  $mnt/tmp/tb             &&\
-  /bin/mount -o bind,ro    /usr/portage        $mnt/usr/portage        &&\
-  /bin/mount -t tmpfs      tmpfs -o size=16G   $mnt/var/tmp/portage    &&\
-  /bin/mount -o bind       /var/tmp/distfiles  $mnt/var/tmp/distfiles
+  # tinderbox data dir
+  #
+  /bin/mount -o bind      /home/tinderbox/tb  $mnt/tmp/tb             &&\
+  #
+  # host repo and more
+  #
+  /bin/mount -o bind,ro   /usr/portage        $mnt/usr/portage        &&\
+  /bin/mount -t tmpfs     tmpfs -o size=16G   $mnt/var/tmp/portage    &&\
+  /bin/mount -o bind      /var/tmp/distfiles  $mnt/var/tmp/distfiles
 
   return $?
 }
 
 
-# if an umount fails then try to umount as much as possible
-#
 function umountall()  {
+  # if an umount fails then try to umount as much as possible
+  #
   rc=0
 
   /bin/umount -l $mnt/dev{/pts,/shm,/mqueue,}     || rc=$?
@@ -43,6 +49,24 @@ function umountall()  {
   /bin/umount -l $mnt/var/tmp/{distfiles,portage} || rc=$?
 
   return $rc
+}
+
+
+# CGroup based limitations to avoid oom-killer eg. for dev-perl/GD
+#
+function cgroup() {
+  sysfsdir=/sys/fs/cgroup/memory/tinderbox-$(basename $mnt)
+  if [[ ! -d $sysfsdir ]]; then
+    mkdir -p $sysfsdir
+  fi
+
+  echo "$$" > $sysfsdir/tasks
+
+  mbytes="$(echo " 8 * 2^30" | bc)"
+  echo $mbytes > $sysfsdir/memory.limit_in_bytes
+
+  vbytes="$(echo "16 * 2^30" | bc)"
+  echo $vbytes > $sysfsdir/memory.memsw.limit_in_bytes
 }
 
 
@@ -68,7 +92,7 @@ if [[ ! -d "$mnt" ]]; then
   exit 1
 fi
 
-# 1st barrier to prevent starting a chroot image twice: a lock file
+# 1st barrier to prevent starting the same chroot image twice: a lock file
 #
 lock=$mnt/tmp/LOCK
 if [[ -f $lock ]]; then
@@ -77,32 +101,16 @@ if [[ -f $lock ]]; then
 fi
 touch $lock || exit 2
 
-# 2nd barrier to prevent starting a chroot image twice: grep mount table
-# this is a weak condition b/c a mount can be made using a symlink name
+# 2nd barrier to prevent starting the same chroot image twice: grep mount table
+# this is a weak condition b/c a mount could be made using a symlink name
 #
 grep -m 1 "$(basename $mnt)" /proc/mounts && exit 3
 
-# mount the directories shared by the host
-#
 mountall || exit 4
-
-# cgroup based limitations to avoid oom-killer eg. for dev-perl/GD
-#
-sysfsdir=/sys/fs/cgroup/memory/tinderbox-$(basename $mnt)
-if [[ ! -d $sysfsdir ]]; then
-  mkdir -p $sysfsdir
-fi
-
-echo "$$" > $sysfsdir/tasks
-
-mbytes="$(echo " 8 * 2^30" | bc)"
-echo $mbytes > $sysfsdir/memory.limit_in_bytes
-
-vbytes="$(echo "16 * 2^30" | bc)"
-echo $vbytes > $sysfsdir/memory.memsw.limit_in_bytes
+cgroup || exit 4
 
 if [[ $# -gt 0 ]]; then
-  # su to local root to run with its *image* environment
+  # do "su - root" to double ensure to use the chroot image environment
   #
   /usr/bin/chroot $mnt /bin/bash -l -c "su - root -c '$@'"
 else
