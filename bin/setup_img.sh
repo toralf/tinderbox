1c46385e405921ae0b7607c50a29cea836e1fc90
Toralf Förster <toralf.foerster@gmx.de>
2017-12-31 00:01:34 +0100

    bin/setup_img.sh: sys-devel/flex needs to be rebuild after the 13.0->17.0 profile switch
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -492,6 +492,13 @@ USE="\${USE} libressl -gnutls -openssl"
 EOF
   fi
 
+  # 13.0 -> 17.0 profile switch needs at least: emerge -p1 $(find /usr/ -type f -name '*.a')
+  # fortunately GCC upgrade makes most of the work already
+  #
+  cat << EOF >> $backlog.1st
+%emerge -1 sys-devel/flex
+EOF
+
   # update GCC asap after setup
   #
   cat << EOF >> $backlog.1st
