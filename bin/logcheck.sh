c4386d783477087dd2c62ca6c70358b01efaefd8
Toralf Förster <toralf.foerster@gmx.de>
2018-01-01 16:50:56 +0100

    bin/logcheck.sh: remove the while and call it via cron
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/logcheck.sh
+++ bin/logcheck.sh
@@ -2,28 +2,24 @@
 #
 # set -x
 
-# check for non-empty image log files
+# check that logs and nohup.out are empty
 #
 
 mailto="tinderbox@zwiebeltoralf.de"
 
 f=/tmp/$(basename $0).out
 
-while :
-do
-  if [[ ! -f $f ]]; then
-    if [[ -s ~/nohup.out ]]; then
-      (ls -l ~/nohup.out; head -n 500 ~/nohup.out) | mail -s "nohup.out is non-empty" $mailto
-      truncate -s 0 ~/nohup.out
-    fi
-
-    if [[ -n "$(ls ~/logs/)" && "$(wc -c ~/logs/* 2>/dev/null | tail -n 1 | awk ' { print $1 } ')" != "0" ]]; then
-      ls -l ~/logs/* >> $f
-      head ~/logs/*  >> $f
-      echo -e "\nto re-activate this test again, do:  truncate -s 0 logs/*; rm -f $f" >> $f
-
-      cat $f | mail -s "logs are non-empty" $mailto
-    fi
+if [[ ! -f $f ]]; then
+  if [[ -s ~/nohup.out ]]; then
+    (ls -l ~/nohup.out; head -n 500 ~/nohup.out) | mail -s "nohup.out is NOT empty" $mailto
+    truncate -s 0 ~/nohup.out
   fi
-  sleep 60
-done
+
+  if [[ -n "$(ls ~/logs/)" && "$(wc -c ~/logs/* 2>/dev/null | tail -n 1 | awk ' { print $1 } ')" != "0" ]]; then
+    ls -l ~/logs/* >> $f
+    head ~/logs/*  >> $f
+    echo -e "\nto re-activate this test again, do:  truncate -s 0 logs/*; rm -f $f" >> $f
+
+    cat $f | mail -s "logs are non-empty" $mailto
+  fi
+fi
