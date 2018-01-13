a23c45df98e8fac06df99796fa5a2fac75aa1d62
Toralf Förster <toralf.foerster@gmx.de>
2018-01-13 20:09:48 +0100

    bin/setup_img.sh: filter out USE flag prefix
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -22,7 +22,7 @@ function ThrowUseFlags()  {
   grep -h -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use{,.local}.desc |\
   cut -f2 -d ':' |\
   cut -f1 -d ' ' |\
-  egrep -v -e '32|64|^armv|bindist|build|cdinstall|gcj|hostname|kill|linguas|make-symlinks|minimal|monolithic|multilib|musl|oci8|pax|qt4|tools|selinux|static|symlink|systemd|test|uclibc|vim-syntax' |\
+  egrep -v -e '32|64|^armv|bindist|build|cdinstall|gcj|hostname|kill|linguas|make-symlinks|minimal|monolithic|multilib|musl|oci8|pax|prefix|qt4|tools|selinux|static|symlink|systemd|test|uclibc|vim-syntax' |\
   sort -u --random-sort |\
   head -n $(($RANDOM % $n)) |\
   sort |\
