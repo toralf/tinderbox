e5a1eb0f41e74dbe580caf6a38ca729203c67e0f
Toralf Förster <toralf.foerster@gmx.de>
2018-02-25 13:46:52 +0100

    bin/scw.sh: simplify whole logic to the minimum
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/scw.sh
+++ bin/scw.sh
@@ -1,33 +1,14 @@
-# #!/bin/sh
+#!/bin/sh
 #
 # set -x
 
-# this is a (s)imple (c)hroot (w)rapper to chroot into a *running* tinderbox image
-# it will not mount any file systems like /dev, /proc or so
-
-if [[ $# -eq 0 ]]; then
-  echo
-  echo " an image name is expected !"
-  echo
-  exit 1
-fi
+# this is a (s)imple (c)hroot (w)rapper into a (running) tinderbox image
 
 mnt=$1
-shift
 
-# guess a directory if just the name is given
+# remaining options are treated as a complete command line to be run within chroot
 #
-if [[ ! -d $mnt ]]; then
-  tmp=$(ls -d /home/tinderbox/img?/$mnt 2>/dev/null)
-  if [[ ! -d $tmp ]]; then
-    echo
-    echo " cannot guess the full path to the image $mnt"
-    echo
-    exit 1
-  fi
-
-  mnt=$tmp
-fi
+shift
 
 if [[ $# -gt 0 ]]; then
   # do "su - root" to double ensure to use the chroot image environment
