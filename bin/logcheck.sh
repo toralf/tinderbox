79a250499e7b90748b40da18d0e7a93848a6aa06
Toralf Förster <toralf.foerster@gmx.de>
2018-05-25 23:07:44 +0200

    bin/logcheck.sh: add a while :; sleep 15 loop
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/logcheck.sh
+++ bin/logcheck.sh
@@ -9,17 +9,22 @@ mailto="tinderbox@zwiebeltoralf.de"
 
 f=/tmp/$(basename $0).out
 
-if [[ ! -f $f ]]; then
-  if [[ -s ~/nohup.out ]]; then
-    (ls -l ~/nohup.out; head -n 500 ~/nohup.out) | mail -s "nohup.out is NOT empty" $mailto
-    truncate -s 0 ~/nohup.out
+while :
+do
+  if [[ ! -f $f ]]; then
+    if [[ -s ~/nohup.out ]]; then
+      (ls -l ~/nohup.out; head -n 500 ~/nohup.out) | mail -s "nohup.out is NOT empty" $mailto
+      truncate -s 0 ~/nohup.out
+    fi
+
+    if [[ -n "$(ls ~/logs/)" && "$(wc -c ~/logs/* 2>/dev/null | tail -n 1 | awk ' { print $1 } ')" != "0" ]]; then
+      ls -l ~/logs/* >> $f
+      head ~/logs/*  >> $f
+      echo -e "\n\n\nto re-activate this test again, do:\n\n   truncate -s 0 logs/*; rm -f $f" >> $f
+
+      cat $f | mail -s "logs are non-empty" $mailto
+    fi
   fi
 
-  if [[ -n "$(ls ~/logs/)" && "$(wc -c ~/logs/* 2>/dev/null | tail -n 1 | awk ' { print $1 } ')" != "0" ]]; then
-    ls -l ~/logs/* >> $f
-    head ~/logs/*  >> $f
-    echo -e "\n\n\nto re-activate this test again, do:\n\n   truncate -s 0 logs/*; rm -f $f" >> $f
-
-    cat $f | mail -s "logs are non-empty" $mailto
-  fi
-fi
+  sleep 15
+done
