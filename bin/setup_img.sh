e9f7b88cf7fe3fc47c05bd4932d5328afd823961
Toralf Förster <toralf.foerster@gmx.de>
2018-07-22 18:14:51 +0200

    bin/setup_img.sh: set USE=-ssl for qtnetwork for libressl images
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -386,6 +386,7 @@ EOF
     cat << EOF > ./tmp/00libressl
 */*               libressl -gnutls -openssl
 net-misc/curl     curl_ssl_libressl -curl_ssl_gnutls -curl_ssl_openssl
+>=dev-qt/qtnetwork-5.11.1 -ssl
 EOF
   fi
 
