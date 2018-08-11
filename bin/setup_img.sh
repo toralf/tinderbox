a00967e58ebc546d708f02c855bd35335c1f4b14
Toralf Förster <toralf.foerster@gmx.de>
2018-08-11 10:07:43 +0200

    bin/setup_img.sh: avoid USE=ghcbootstrap and be less verbose
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -22,7 +22,7 @@ function ThrowUseFlags()  {
   grep -h -v -e '^$' -e '^#' -e 'internal use only' -e 'DO NOT USE THIS' /usr/portage/profiles/use{,.local}.desc |\
   cut -f2 -d ':' |\
   cut -f1 -d ' ' |\
-  egrep -v -e '32|64|^armv|bindist|build|cdinstall|debug|gallium|gcj|hostname|kill|linguas|make-symlinks|minimal|monolithic|multilib|musl|nvidia|oci8|opencl|pax|prefix|qt4|tools|selinux|ssl|static|symlink|systemd|test|uclibc|vaapi|vdpau|vim-syntax|vulkan' |\
+  egrep -v -e '32|64|^armv|bindist|build|cdinstall|debug|gallium|gcj|ghcbootstrap|hostname|kill|linguas|make-symlinks|minimal|monolithic|multilib|musl|nvidia|oci8|opencl|pax|prefix|qt4|tools|selinux|ssl|static|symlink|systemd|test|uclibc|vaapi|vdpau|vim-syntax|vulkan' |\
   sort -u --random-sort |\
   head -n $(($RANDOM % $n)) |\
   sort |\
@@ -736,7 +736,6 @@ do
     esac
   done
 
-  echo -n "."
   CheckOptions
   ComputeImageName
 
