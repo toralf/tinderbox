a66546e22bf3cb76db029501dbde7421e96d06c6
Toralf Förster <toralf.foerster@gmx.de>
2018-09-17 21:33:56 +0200

    bin/replace_img.sh: accept parameter (in hours)
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/replace_img.sh
+++ bin/replace_img.sh
@@ -4,15 +4,53 @@
 
 # replace an tinderbox image with a new one
 #
-
-# get the oldest image (symlink) name
+# works together with a crontab entry like:
 #
-i=$( ls -1td ~/run/* 2>/dev/null | tail -n 1 | xargs -n 1 basename 2>/dev/null )
+# # spin up a new tinderbox image after N hours
+# #
+# 1 * * * * cd ~/img2; /opt/tb/bin/replace_img.sh 20
 
-if [[ ! -e ~/run/${i} ]]; then
+
+n=$( pgrep -c $(basename $0) )
+if [[ ${n} -ne 1 ]]; then
+  echo "found ${n} running instances of us"
+  pgrep -a $(basename $0)
   exit 1
 fi
 
+# if $1 is given, then wait till the newest image is older than $1 (in hours)
+#
+if [[ $# -ne 0 ]]; then
+  while :
+  do
+    # get the newest image
+    #
+    i=$( ls -1td ~/run/* 2>/dev/null | head -n 1 | xargs -n 1 basename 2>/dev/null )
+    if [[ -z "${i}" ]]; then
+      echo "no newest image found"
+      exit 2
+    fi
+
+    # get age in hours
+    #
+    let "age = $(date +%s) - $(stat -c%Y ~/run/${i})"
+    let "age = $age / 3600"
+    if [[ $age -ge $1 ]]; then
+      break
+    fi
+    exit 3
+  done
+fi
+
+# kick off the oldest image
+#
+i=$( ls -1td ~/run/* 2>/dev/null | tail -n 1 | xargs -n 1 basename 2>/dev/null )
+if [[ -z "${i}" ]]; then
+  echo "no oldest image found"
+  exit 4
+fi
+date
+echo "stopping ${i}"
 /opt/tb/bin/stop_img.sh ${i}
 
 # wait till it is stopped
@@ -22,12 +60,16 @@ do
   if [[ ! -f ~/run/${i}/tmp/LOCK ]]; then
     break
   fi
-  sleep 10
+  sleep 1
 done
+rm ~/run/${i} ~/logs/${i}.log
+date
+echo "deleted  ${i}"
+echo
 
-rm ~/run/${i} ~/log/${i}.log
-
+# spin up a new one
+#
 while :
 do
-  /opt/tb/bin/setup_img.sh && break
+  sudo /opt/tb/bin/setup_img.sh && break
 done
