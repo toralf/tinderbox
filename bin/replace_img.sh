49410d32df4e243e42de879b65c560a5709801a0
Toralf Förster <toralf.foerster@gmx.de>
2018-09-28 20:14:26 +0200

    bin/replace_img.sh: par1 (in hours) is now mandatory
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/replace_img.sh
+++ bin/replace_img.sh
@@ -8,87 +8,92 @@
 #
 # # spin up a new tinderbox image after N hours
 # #
-# 1 * * * * cd ~/img2; /opt/tb/bin/replace_img.sh 20
+# 1 * * * * cd ~/img2; /opt/tb/bin/replace_img.sh 24
 
+
+if [[ $# -ne 1 ]]; then
+  echo "missing mandatory parameter1: <hours>, exiting..."
+  exit 1
+fi
 
 n=$( pgrep -c $(basename $0) )
 if [[ ${n} -ne 1 ]]; then
-  echo "found ${n} running instances of us"
+  echo "found ${n} running instances beside us, exiting..."
   pgrep -a $(basename $0)
   exit 1
 fi
 
-# if $1 is given, then wait till the newest image is older than $1 (in hours)
+# get the newest image
 #
-if [[ $# -ne 0 ]]; then
+nimg=$( ls -1td ~/run/* 2>/dev/null | head -n 1 | xargs -n 1 basename 2>/dev/null )
+if [[ -z "${nimg}" ]]; then
+  echo "no newest image found, exiting..."
+  exit 2
+fi
+
+# bail out if its age is below $1 hours
+#
+let "age = $(date +%s) - $(stat -c%Y ~/run/${nimg})"
+let "age = $age / 3600"
+if [[ $age -lt $1 ]]; then
+  exit 3
+fi
+
+# kick off the oldest image if there's one
+#
+oimg=$( ls -1td ~/run/* 2>/dev/null | tail -n 1 | xargs -n 1 basename 2>/dev/null )
+if [[ -e "${oimg}" ]]; then
+  # bail out if its age is below N days
+  #
+  let "age = $(date +%s) - $(stat -c%Y ~/run/${oimg})"
+  let "age = $age / 86400"
+  if [[ $age -lt 8 ]]; then
+    exit 4
+  fi
+
+  echo
+  date
+  /opt/tb/bin/stop_img.sh ${oimg}
+  # wait till the old image is stopped
+  #
   while :
   do
-    # get the newest image
-    #
-    i=$( ls -1td ~/run/* 2>/dev/null | head -n 1 | xargs -n 1 basename 2>/dev/null )
-    if [[ -z "${i}" ]]; then
-      echo "no newest image found"
-      exit 2
-    fi
-
-    # get age in hours
-    #
-    let "age = $(date +%s) - $(stat -c%Y ~/run/${i})"
-    let "age = $age / 3600"
-    if [[ $age -ge $1 ]]; then
+    if [[ ! -f ~/run/${oimg}/tmp/LOCK ]]; then
       break
     fi
-    exit 3
+    sleep 1
   done
+  rm ~/run/${oimg} ~/logs/${oimg}.log
+  date
+  echo "deleted  ${oimg}"
 fi
 
-# kick off the oldest image
-#
-i=$( ls -1td ~/run/* 2>/dev/null | tail -n 1 | xargs -n 1 basename 2>/dev/null )
-if [[ -z "${i}" ]]; then
-  echo "no oldest image found"
-  exit 4
-fi
-date
-echo "stopping ${i}"
-/opt/tb/bin/stop_img.sh ${i}
-
-# wait till the old image is stopped
-#
-while :
-do
-  if [[ ! -f ~/run/${i}/tmp/LOCK ]]; then
-    break
-  fi
-  sleep 1
-done
-rm ~/run/${i} ~/logs/${i}.log
-date
-echo "deleted  ${i}"
-echo
-
-# spin up a new image
+# spin up a new image, more than 1 attempt might be needed
 #
 i=0
+tmpfile=$(mktemp /tmp/$(basename $0).XXXXXX)
 while :
 do
   let "i = ${i} + 1"
 
-  # catch this output in a separate mail
-  #
-  tmpfile=$(mktemp /tmp/$(basename $0).XXXXXX)
   sudo /opt/tb/bin/setup_img.sh &> ${tmpfile}
   rc=$?
-  cat ${tmpfile} | mail -s "admin: $(basename $0): attempt ${i} has rc=${rc}" tinderbox@zwiebeltoralf.de
   if [[ ${rc} -eq 0 ]]; then
     break
+  else
+    cat ${tmpfile} | mail -s "admin: $(basename $0) attempt ${i} rc=${rc}" tinderbox@zwiebeltoralf.de
   fi
-  rm -f ${tmpfile}
 
   if [[ ${i} -gt 10 ]]; then
-    echo "$(basename $0)" | mail -s "admin: ${i} attempts w/o success, going into deep sleep ..." tinderbox@zwiebeltoralf.de
-    # block next attempt till this sleep is killed
+    # retry hourly
     #
-    sleep 86400123
+    sleep 3600
   fi
 done
+cat ${tmpfile}
+rm -f ${tmpfile}
+
+
+echo
+date
+echo "done, needed ${i} attempt(s)"
