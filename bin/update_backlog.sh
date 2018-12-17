31b4ad117f77b4518ca817d37a1401af193a22d5
Toralf Förster <toralf.foerster@gmx.de>
2018-12-17 10:19:21 +0100

    bin/update_backlog.sh: reformat pipe
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/update_backlog.sh
+++ bin/update_backlog.sh
@@ -19,7 +19,11 @@ cd /var/db/repos/gentoo
 # add 2 hours to let mirrors be in sync
 #
 git diff --diff-filter=ACMR --name-status "@{ ${1:-3} hour ago }".."@{ 2 hour ago }" 2>/dev/null |\
-grep -F -e '/files/' -e '.ebuild' | cut -f2- -s | xargs -n 1 | cut -f1-2 -d'/' -s | sort --unique |\
+grep -F -e '/files/' -e '.ebuild'   |\
+cut -f2- -s                         |\
+xargs -n 1                          |\
+cut -f1-2 -d'/' -s                  |\
+sort --unique                       |\
 grep -v -f ~/tb/data/IGNORE_PACKAGES > $acmr
 
 # add latest changes to each backlog.upd
@@ -27,7 +31,7 @@ grep -v -f ~/tb/data/IGNORE_PACKAGES > $acmr
 if [[ -s $acmr ]]; then
   for i in $(ls ~/run 2>/dev/null)
   do
-    # randomizing lowers probability of parallel build of the same package
+    # shuffle around to lower probability of parallel build of the same package
     #
     bl=~/run/$i/tmp/backlog.upd
     sort --unique --random-sort $bl $acmr > $bl.tmp && cp $bl.tmp $bl && rm $bl.tmp
