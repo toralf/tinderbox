a17787a83ae2fc38b6dda07578a4072dd9e48557
Toralf Förster <toralf.foerster@gmx.de>
2018-12-27 22:43:43 +0100

    bin/update_backlog.sh: ignore git "R" ebuilds
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/update_backlog.sh
+++ bin/update_backlog.sh
@@ -10,31 +10,31 @@ if [[ ! "$(whoami)" = "tinderbox" ]]; then
   exit 1
 fi
 
-# list of added/changed/modified/renamed ebuilds
+# list of package(s) to add
 #
-acmr=/tmp/$(basename $0).acmr
+pks=/tmp/$(basename $0).txt
 
 repo_path=$( portageq get_repo_path / gentoo ) || exit 2
-cd $repo_path
+cd $repo_path || exit 3
 
 # add 2 hours to let mirrors be in sync
 #
-git diff --diff-filter=ACMR --name-status "@{ ${1:-2} hour ago }".."@{ 1 hour ago }" 2>/dev/null |\
+git diff --diff-filter=ACM --name-status "@{ ${1:-2} hour ago }".."@{ 1 hour ago }" 2>/dev/null |\
 grep -F -e '/files/' -e '.ebuild'   |\
 cut -f2- -s                         |\
 xargs -n 1                          |\
 cut -f1-2 -d'/' -s                  |\
 sort --unique                       |\
-grep -v -f ~/tb/data/IGNORE_PACKAGES > $acmr
+grep -v -f ~/tb/data/IGNORE_PACKAGES > $pks
 
 # add latest changes to each backlog.upd
 #
-if [[ -s $acmr ]]; then
+if [[ -s $pks ]]; then
   for i in $(ls ~/run 2>/dev/null)
   do
-    # shuffle around to lower probability of parallel build of the same package
+    # shuffle around lowers the probability to build in parallel the same package
     #
     bl=~/run/$i/tmp/backlog.upd
-    sort --unique --random-sort $bl $acmr > $bl.tmp && cp $bl.tmp $bl && rm $bl.tmp
+    sort --unique --random-sort $bl $pks > $bl.tmp && cp $bl.tmp $bl && rm $bl.tmp
   done
 fi
