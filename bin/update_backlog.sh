26192dcb86df70cba062f030df809d2c4a2549f9
Toralf Förster <toralf.foerster@gmx.de>
2019-02-17 11:40:18 +0100

    comments
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/update_backlog.sh
+++ bin/update_backlog.sh
@@ -17,10 +17,10 @@ pks=/tmp/$(basename $0).txt
 repo_path=$( portageq get_repo_path / gentoo ) || exit 2
 cd $repo_path || exit 3
 
-# add 2 hours to let mirrors be in sync
+# add 1 hour to let mirrors be in sync
 #
 git diff --diff-filter=ACM --name-status "@{ ${1:-2} hour ago }".."@{ 1 hour ago }" 2>/dev/null |\
-grep -F -e '/files/' -e '.ebuild' -e 'Manifest'               |\
+grep -F -e '/files/' -e '.ebuild' -e 'Manifest'                                                 |\
 cut -f2- -s | xargs --no-run-if-empty -n 1 | cut -f1-2 -d'/' -s | sort --unique |\
 grep -v -f ~/tb/data/IGNORE_PACKAGES > $pks
 
@@ -29,8 +29,6 @@ grep -v -f ~/tb/data/IGNORE_PACKAGES > $pks
 if [[ -s $pks ]]; then
   for i in $(ls ~/run 2>/dev/null)
   do
-    # shuffle around to lower the probability of a parallel build of the same package
-    #
     bl=~/run/$i/tmp/backlog.upd
     sort --unique $bl $pks | shuf > $bl.tmp
     # use cp+rm instead of mv to keep permissions of $bl
