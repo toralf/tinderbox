toralf/tinderbox

commit 3b334898354b2717ed931aaa381858c05011dc75

bin/job.sh: replace a while with xargs
Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

bin/job.sh
@@ -809,12 +809,9 @@ function SendoutIssueMail()  {
 #
 function PutDepsIntoWorldFile() {
   emerge --depclean --pretend --verbose=n 2>/dev/null |\
-  grep "^All selected packages: " |\
-  cut -f2- -d':' -s |\
-  while read p
-  do
-    emerge -O --noreplace $p &>/dev/null
-  done
+  grep "^All selected packages: "                     |\
+  cut -f2- -d':' -s                                   |\
+  xargs --no-run-if-empty emerge -O --noreplace
 }
 
 

commit ecae40673e8a156520c90648bf9718f3568d0ddf

bin/job.sh: fix verbosity bug introduced in 3b33489835
Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

bin/job.sh
@@ -9,7 +9,7 @@
 # That's all.
 
 
-# strip away non-printable sequences
+# strip away non-printable chars
 #
 function stresc() {
   perl -MTerm::ANSIColor=colorstrip -nle '
@@ -809,7 +809,7 @@ function PutDepsIntoWorldFile() {
   emerge --depclean --pretend --verbose=n 2>/dev/null |\
   grep "^All selected packages: "                     |\
   cut -f2- -d':' -s                                   |\
-  xargs --no-run-if-empty emerge -O --noreplace
+  xargs --no-run-if-empty emerge -O --noreplace &>/dev/null
 }
 
 
@@ -1186,7 +1186,7 @@ function WorkOnTask() {
   pkglog=""
   pkgname=""
 
-  # image update
+  # @set
   #
   if [[ $task =~ ^@ ]]; then
     opts=""
@@ -1265,7 +1265,7 @@ function WorkOnTask() {
   elif [[ $task =~ ^= ]]; then
     RunAndCheck "emerge $task"
 
-  # straight package or a @set
+  # straight package name
   #
   else
     RunAndCheck "emerge --update $task"