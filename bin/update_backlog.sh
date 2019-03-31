adc5af2d38b83a1c10a6b1aba0d813bc65c8c055
Toralf Förster <toralf.foerster@gmx.de>
2019-03-31 10:27:37 +0200

    bin/update_backlog.sh: filter out IGNORE packages
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/update_backlog.sh
+++ bin/update_backlog.sh
@@ -20,8 +20,10 @@ pks=/tmp/$(basename $0).txt
 # default: 1 hour to let mirrors be synced
 #
 git diff --diff-filter=ACM --name-status "@{ ${1:-2} hour ago }".."@{ ${1:-1} hour ago }" 2>/dev/null |\
-grep -F -e '/files/' -e '.ebuild' -e 'Manifest'                                                 |\
-cut -f2- -s | xargs -n 1 | cut -f1-2 -d'/' -s | sort --unique > $pks
+grep -F -e '/files/' -e '.ebuild' -e 'Manifest' |\
+cut -f2- -s | xargs -n 1 | cut -f1-2 -d'/' -s   |\
+grep -v -f ~/tb/data/IGNORE_PACKAGES            |\
+sort --unique > $pks
 
 if [[ -s $pks ]]; then
   for i in $(ls ~/run 2>/dev/null)
