d3d56cbf400764366cf189e0b3105a1b1efbc5ac
Toralf Förster <toralf.foerster@gmx.de>
2019-05-01 19:04:19 +0200

    bin/setup_img.sh: reorder EMERGE_DEFAULT_OPTS and set autounmask=n
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -352,7 +352,7 @@ $( [[ "$multilib" = "y" ]] && echo 'ABI_X86="32 64"' )
 ACCEPT_KEYWORDS=$( [[ "$keyword" = "unstable" ]] && echo '"~amd64"' || echo '"amd64"' )
 
 FEATURES="$features"
-EMERGE_DEFAULT_OPTS="--verbose --verbose-conflicts --color=n --nospinner --tree --quiet-build --with-bdeps=y --complete-graph=y --backtrack=500 --autounmask-keep-masks=y"
+EMERGE_DEFAULT_OPTS="--with-bdeps=y --verbose-conflicts --nospinner --tree --quiet-build --autounmask-keep-masks=y --complete-graph=y --backtrack=500 --verbose --color=n --autounmask=n"
 ACCEPT_PROPERTIES="-interactive"
 ACCEPT_RESTRICT="-fetch"
 CLEAN_DELAY=0
