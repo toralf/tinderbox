8c013c41d68f7ac46f0c39139543bc114d7cbe37
Toralf Förster <toralf.foerster@gmx.de>
2019-06-11 20:00:25 +0200

    bin/replace_img.sh: go back to 5 days/17 hours
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/replace_img.sh
+++ bin/replace_img.sh
@@ -112,8 +112,8 @@ echo $$ > $lck
 
 oldimg=$(basename $1 2>/dev/null)
 if [[ -z "$oldimg" || ! -e ~/run/$oldimg ]]; then
-  min_days=${1:-6}
-  min_hours=${2:-11}
+  min_days=${1:-5}
+  min_hours=${2:-17}
   min_compl=${3:-4500}
   shift "$(( $# < 3 ? $# : 3 ))"
 
