4b50ac7771ef1af72cfc7938ae71f6530f8b2496
Toralf Förster <toralf.foerster@gmx.de>
2019-07-10 21:26:49 +0200

    bin/update_backlog.sh: prettify source code
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/update_backlog.sh
+++ bin/update_backlog.sh
@@ -19,23 +19,24 @@ pks=/tmp/${0##*/}.txt
 
 # default: 1 hour to let mirrors be synced
 #
-git diff --diff-filter=ACM --name-status "@{ ${1:-2} hour ago }".."@{ ${1:-1} hour ago }" 2>/dev/null |\
+git diff --diff-filter=ACM --name-status "@{ 2 hour ago }".."@{ 1 hour ago }" 2>/dev/null |\
 grep -F -e '/files/' -e '.ebuild' -e 'Manifest' |\
 cut -f2- -s | xargs -n 1 | cut -f1-2 -d'/' -s   |\
 grep -v -f ~/tb/data/IGNORE_PACKAGES            |\
-sort --unique > $pks
+sort -u > $pks
 
 if [[ -s $pks ]]; then
   for i in $(ls ~/run 2>/dev/null)
   do
-    # fs is not (yet) mounted)?
+    # is ~/img? file system mounted?
+    #
     if [[ ! -e ~/run/$i/var/tmp/tb/ ]]; then
       continue
     fi
 
     bl=~/run/$i/var/tmp/tb/backlog.upd
-    sort --unique $bl $pks | shuf > $bl.tmp
-    # "mv" overwrites file permissions
+    sort -u $bl $pks | shuf > $bl.tmp
+    # no "mv", that overwrites file permissions
     #
     cp $bl.tmp $bl
     rm $bl.tmp
