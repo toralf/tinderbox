3fc1a95107c8bd06fbd0a1fe26f074b9ba543cb8
Toralf Förster <toralf.foerster@gmx.de>
2019-10-12 10:38:26 +0200

    bin/whatsup.sh: fix misleading column title
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/whatsup.sh
+++ bin/whatsup.sh
@@ -75,7 +75,7 @@ function Overall() {
   done
   inrun=$(wc -w <<< $images)
 
-  echo "compl fail  days backlog  upd  1st state  $running running, $inrun in ~/run"
+  echo "compl fail  days backlog  upd  1st state  $running#$inrun running"
 
   for i in $images
   do
