2804607e0b1e1648326322dd7c9000b00c79157d
Toralf Förster <toralf.foerster@gmx.de>
2020-07-12 23:53:38 +0200

    bin/cgroup.sh: use cgset instead echo
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- bin/cgroup.sh
+++ bin/cgroup.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 # set -x
 
-# global upper limits for tinderboxes et.a l.
+# global upper limits for tinderboxes, fuzzer et. al.
 
-# set -euf
+set -euf
 
 # hold lockdir names, the locking mechanism used by the tinderbox script bwrap.sh
 mkdir /run/tinderbox
+
+echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/cpu/release_agent
+echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/memory/release_agent
 
 name=/local
 
 cgcreate -g cpu,memory:$name
 
-cgset -r cpu.cfs_quota_us=900000  $name
+cgset -r cpu.use_hierarchy=1      $name
+cgset -r cpu.cfs_quota_us=800000  $name
 cgset -r cpu.cfs_period_us=100000 $name
+cgset -r cpu.notify_on_release=1  $name
 
 cgset -r memory.use_hierarchy=1           $name
 cgset -r memory.limit_in_bytes=120G       $name
 cgset -r memory.memsw.limit_in_bytes=140G $name
-
-echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/cpu/release_agent
-echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/memory/release_agent
-
-echo 1 > /sys/fs/cgroup/cpu/$name/notify_on_release
-echo 1 > /sys/fs/cgroup/memory/$name/notify_on_release
+cgset -r memory.notify_on_release=1       $name
