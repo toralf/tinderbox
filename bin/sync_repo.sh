191cd044dea35167cc71247eeae98089d6ff8852
Toralf Förster <toralf.foerster@gmx.de>
2020-12-12 12:34:46 +0100

    bin/sync_repo.sh: add "set -e" and be more relax if "git pull" fails

--- bin/sync_repo.sh
+++ bin/sync_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#set -x
+# set -x
 
-set -uf
+set -euf
 
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin:/opt/tb/bin"
 export LANG=C.utf8
@@ -18,37 +18,26 @@ log="/tmp/${0##*/}.log"
 
 date > $log || exit 1
 eix-sync &>> $log
-rc1=$?
 
 # these repos are not used at the tinderbox host itself so eix-sync won't sync them
-rc2=0
 for repo in musl science
 do
   date >> $log
   (cd /var/db/repos/$repo && git pull) &>> $log
-  ((rc2=rc2+$?))
 done
 
-if [[ $rc1 -ne 0 || $rc2 -ne 0 || -n "$(grep 'git pull error' $log)" ]]; then
-  mail -s "${0##*/}: return codes: eix=$rc1 musl=$rc2" $mailto < $log
-  exit 1
-fi
-
-# timestamp can't be queried within an image b/c git isn't part of stage3 and might not yet be installed
-#
-for repo in gentoo libressl musl
+# needed by job.sh
+for repo in $(ls /var/db/repos/)
 do
-  cd /var/db/repos/$repo && git show -s --format="%ct" HEAD > timestamp.git
+  [[ -d /var/db/repos/$repo/.git ]] && cd /var/db/repos/$repo && git show -s --format="%ct" HEAD > timestamp.git
 done
 
 echo  >> $log
 date  >> $log
 
-grep -q "warning: There are too many unreachable loose objects; run 'git prune' to remove them." $log
-if [[ $? -eq 0 ]]; then
-  for repo in gentoo libressl musl
+if grep -q "warning: There are too many unreachable loose objects; run 'git prune' to remove them." $log; then
+  for repo in $(ls /var/db/repos/)
   do
     cd /var/db/repos/$repo && git prune &>> $log
   done
 fi
-
