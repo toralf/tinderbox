84e8173b1bdead3ec4d071902aab7e56550e7854
Toralf Förster <toralf.foerster@gmx.de>
2020-12-27 16:48:20 +0100

    bin/cgroup.sh: make the release agent script available for all local users

--- bin/cgroup.sh
+++ bin/cgroup.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 # set -x
 
-# global upper limits for tinderboxes, fuzzer et. al.
-# call this script eg. by a crontab @reboot line
+# set cgroup v1 limits
 
 set -euf
 
-echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/cpu/release_agent
-echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/memory/release_agent
-
+# prefer a generic name b/c not only tinderbox will use it
 name=/local
 
 cgcreate -g cpu,memory:$name
@@ -22,3 +19,12 @@ cgset -r memory.use_hierarchy=1           $name
 cgset -r memory.limit_in_bytes=120G       $name
 cgset -r memory.memsw.limit_in_bytes=140G $name
 cgset -r memory.notify_on_release=1       $name
+
+# make it available for non-tinderbox users too
+cp /opt/tb/bin/cgroup-release-agent.sh /usr/local/bin/
+chmod 755 /usr/local/bin/cgroup-release-agent.sh
+
+for i in cpu memory
+do
+  echo "/usr/local/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/$i/release_agent
+done
