58c80aab02cb94ea87bc48abdd8f848fdbb713e4
Toralf Förster <toralf.foerster@gmx.de>
2020-12-27 11:43:39 +0100

    bin/cgroup.sh: factor out /run/tinderbox creation

--- bin/cgroup.sh
+++ bin/cgroup.sh
@@ -2,14 +2,9 @@
 # set -x
 
 # global upper limits for tinderboxes, fuzzer et. al.
-# the /run directory and the Cgroup settings are used by various scripts
-# so maybe call this script eg. by a crontab @reboot line
+# call this script eg. by a crontab @reboot line
 
 set -euf
-
-if [[ ! -d /run/tinderbox ]]; then
-  mkdir /run/tinderbox
-fi
 
 echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/cpu/release_agent
 echo "/opt/tb/bin/cgroup-release-agent.sh" > /sys/fs/cgroup/memory/release_agent
