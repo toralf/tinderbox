97515fc6104495465694a2e8ab74dd82df579965
Toralf Förster <toralf.foerster@gmx.de>
2021-01-28 17:25:39 +0100

    bin/cgroup.sh:make limits configureable

--- bin/cgroup.sh
+++ bin/cgroup.sh
@@ -13,6 +13,11 @@ if ! hash -r cgcreate || ! hash -r cgset || ! test -d /sys/fs/cgroup; then
   exit 0
 fi
 
+# default: 8 vCPU, 120 GB RAM and 140 GB RAM+swap
+vcpu=$(echo "scale=0; ${1:-8} * 100000" | bc)
+ram=${2:-120G}
+vram=${3:-140G}
+
 # make this script available for non-tinderbox consumers too
 cp /opt/tb/bin/cgroup-release-agent.sh /usr/local/bin/
 chmod 755 /usr/local/bin/cgroup-release-agent.sh
@@ -27,9 +32,6 @@ done
 name=/local
 cgcreate -g cpu,memory:$name
 
-# reserve 8 vCPU for members of $name, period=0.1 sec
-cgset -r cpu.cfs_quota_us=800000  $name
-
-# reserve 120 GB RAM (8GB left) and 140 GB RAM+swap
-cgset -r memory.limit_in_bytes=120G       $name
-cgset -r memory.memsw.limit_in_bytes=140G $name
+cgset -r cpu.cfs_quota_us=$vcpu             $name
+cgset -r memory.limit_in_bytes=$ram         $name
+cgset -r memory.memsw.limit_in_bytes=$vram  $name
