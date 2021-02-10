7e04df9647d83241a8d4ee062d23400f6a331fc5
Toralf Förster <toralf.foerster@gmx.de>
2021-02-10 10:18:07 +0100

    Revert "bin/setup_img.sh: add --complete-graph=y accordingly to bug 731150"
    This reverts commit effca377fa220c7a4c03ee21f5d41fe6030a1582.

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -301,7 +301,7 @@ ACCEPT_PROPERTIES="-interactive"
 ACCEPT_RESTRICT="-fetch"
 
 FEATURES="xattr cgroup -news -collision-protect"
-EMERGE_DEFAULT_OPTS="--with-bdeps=y --verbose-conflicts --nospinner --tree --quiet-build --autounmask-keep-masks=y --complete-graph=y --verbose --color=n --autounmask=n --ask=n --complete-graph=y"
+EMERGE_DEFAULT_OPTS="--with-bdeps=y --verbose-conflicts --nospinner --tree --quiet-build --autounmask-keep-masks=y --complete-graph=y --verbose --color=n --autounmask=n --ask=n"
 
 CLEAN_DELAY=0
 NOCOLOR=true
