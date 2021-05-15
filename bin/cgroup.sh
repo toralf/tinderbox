1713dc3e34123cf91704c794f48d7f27d57b2092
Toralf Förster <toralf.foerster@gmx.de>
2021-05-15 23:33:45 +0200

    bin/cgroup.sh: reserve 3 cores

--- bin/cgroup.sh
+++ bin/cgroup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # set -x
 
-# set cgroup v1 limits
+
+# set overall cgroup v1 limits for all tinderboxes and other fuzzers (==consumers)
+
 
 set -euf
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
@@ -16,12 +18,9 @@ if ! hash -r cgcreate || ! hash -r cgset || ! test -d /sys/fs/cgroup; then
   exit 0
 fi
 
-vcpu=$(echo "( ${1:-$(nproc) - 4} ) * 100000.0" | bc | sed -e 's,\..*,,g')
-ram=${2:-120G}
-vram=${3:-150G}
-
 echo 1 > /sys/fs/cgroup/memory/memory.use_hierarchy
 
+# cgroup v1 does not cleanup after itself
 agent=/tmp/cgroup-release-agent.sh
 cat << EOF > $agent
 #!/bin/sh
@@ -38,6 +37,10 @@ done
 name=/local
 cgcreate -g cpu,memory:$name
 
+# default: reserve 3 cores (SMT gives +60%)
+vcpu=${1:-$(echo "($(nproc) - 3 * 1.6)  * 100000.0" | bc | sed -e 's,\..*,,g')}
+ram=${2:-120G}
+vram=${3:-150G}
 cgset -r cpu.cfs_quota_us=$vcpu             $name
 cgset -r memory.limit_in_bytes=$ram         $name
 cgset -r memory.memsw.limit_in_bytes=$vram  $name
