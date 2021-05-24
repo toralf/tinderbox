2275f6ba0d9bbbcb9649f5258f863342155f4019
Toralf Förster <toralf.foerster@gmx.de>
2021-05-24 22:26:26 +0200

    bin/logcheck.sh: no mail.log check + itnernal no mailx

--- bin/logcheck.sh
+++ bin/logcheck.sh
@@ -3,27 +3,22 @@
 
 # act if stdout/err of job.sh is non-empty
 
-set -eu
+set -u
 export LANG=C.utf8
 
 f=/tmp/${0##*/}.out
 
 while :
 do
-  if [[ ! -f $f ]]; then
+  # if non-empty than it was already reported
+  if [[ ! -s $f ]]; then
     if [[ "$(wc -c ~/logs/*.log 2>/dev/null | tail -n 1 | awk ' { print $1 } ')" != "0" ]]; then
-      (
-        ls -l ~/logs/
-        echo
-        head -v ~/logs/*.log
-        echo
-        ls -l ~/run/*/var/tmp/tb/mail.log
-        echo
-        head -v ~/run/*/var/tmp/tb/mail.log
-        echo -e "\n\nto re-activate this test again, do:\n\n  tail -v ~/logs/*; rm -f $f;     truncate -s 0 ~/logs/*\n\n"
-      ) |\
-      tee $f | mail -s "logs are non-empty" ${MAILTO:-tinderbox} || true
+      ls -l ~/logs/
+      echo
+      head -v ~/logs/*.log
+      echo
+      echo -e "\n\nto re-activate this test again, do:\n\n  tail -v ~/logs/*; rm -f $f;     truncate -s 0 ~/logs/*\n\n"
     fi
   fi
-  sleep 1
+  sleep 30
 done
