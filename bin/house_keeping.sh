a5306174cb297a29f044753df4aad4c1d72cc3b1
Toralf Förster <toralf.foerster@gmx.de>
2021-06-12 13:02:59 +0200

    bin/house_keeping.sh: repeat clean step if needed

--- bin/house_keeping.sh
+++ bin/house_keeping.sh
@@ -7,10 +7,16 @@ export LANG=C.utf8
 
 find ~/distfiles/ -maxdepth 1 -type f -atime +365 -exec rm "{}" \;
 
-if [[ -n $(df -m | awk ' $1 == "/dev/nvme0n1p4" && $4 < 2000000 ') ]]; then
+while [[ -n $(df -m | awk ' $1 == "/dev/nvme0n1p4" && $4 < 2000000 ') ]]
+do
   img=$(ls -d ~/img/* 2>/dev/null | sort -t'-' -k 3,4 | head -n 1)
   if [[ -n $img && -d $img ]]; then
-    echo "will clean up oldest image: $img"
+    echo
+    date
+    df -m /dev/nvme0n1p1
+    echo "prune image: $img"
     rm -rf $img
+    sleep 60              # lazy btrfs
+    df -m /dev/nvme0n1p1
   fi
-fi
+done
