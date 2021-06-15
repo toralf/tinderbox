79f94d85dc4636cc99bad28d15491a81c98bb61b
Toralf Förster <toralf.foerster@gmx.de>
2021-06-15 17:52:12 +0200

    bin/house_keeping.sh: run only as root + fix df

--- bin/house_keeping.sh
+++ bin/house_keeping.sh
@@ -5,18 +5,26 @@
 set -eu
 export LANG=C.utf8
 
-find ~/distfiles/ -maxdepth 1 -type f -atime +365 -exec rm "{}" \;
 
-while [[ -n $(df -m | awk ' $1 == "/dev/nvme0n1p4" && $4 < 2000000 ') ]]
+if [[ "$(whoami)" != "root" ]]; then
+  echo " you must be root"
+  exit 1
+fi
+
+find ~tinderbox/distfiles/ -maxdepth 1 -type f -atime +365 -exec rm "{}" \;
+
+# clean up if either >75% used or <100GB are free
+fs=/dev/nvme0n1p4
+while [[ -n $(df -m | awk ' $1 == "'"$fs"'" && ($3/$2 > 0.75 || $4 < 100000)') ]]
 do
-  img=$(ls -d ~/img/* 2>/dev/null | sort -t'-' -k 3,4 | head -n 1)
-  if [[ -n $img && -d $img ]]; then
+  img=$(ls -d ~tinderbox/img/* 2>/dev/null | sort -t'-' -k 3,4 | head -n 1)
+  if [[ -d $img ]]; then
     echo
     date
-    df -m /dev/nvme0n1p1
+    df -m $fs
     echo "prune image: $img"
     rm -rf $img
     sleep 60              # lazy btrfs
-    df -m /dev/nvme0n1p1
+    df -m $fs
   fi
 done
