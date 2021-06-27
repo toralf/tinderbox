33c83b1992644b2520aa73d2be17119f339d554a
Toralf Förster <toralf.foerster@gmx.de>
2021-06-27 12:20:59 +0200

    bin/index.sh: work with a tmp file and add few more links per bug

--- bin/index.sh
+++ bin/index.sh
@@ -3,21 +3,27 @@
 
 # create ~tinderbox/img/index.html from .reported file in issues directory
 
-index=~tinderbox/img/index.html
 
-truncate -s 0 $index
+set -eu
+export LANG=C.utf8
 
-cat << EOF >> $index
+
+tmpfile=$(mktemp /tmp/$(basename $0)_XXXXXX.tmp)
+
+cat << EOF >> $tmpfile
 <html>
 
-<h1>The current <a href="https://zwiebeltoralf.de/tinderbox.html">tinderbox</a> results.</h1>
+<h1>recent <a href="https://zwiebeltoralf.de/tinderbox.html">tinderbox</a> results</h1>
 
 <table border="0" align="left">
+
   <thead align="left">
     <tr>
       <th>Bug</th>
       <th>Title</th>
-      <th>Image</th>
+      <th>/</th>
+      <th>/etc/portage</th>
+      <th>Issue</th>
     </tr>
   </thead>
 
@@ -25,11 +31,13 @@ cat << EOF >> $index
     <tr>
       <th>Bug</th>
       <th>Title</th>
-      <th>Image</th>
+      <th>/</th>
+      <th>/etc/portage</th>
+      <th>Issue</th>
     </tr>
   </tfoot>
 
-  <tbody align="left">
+  <tbody>
 EOF
 
 ls -t ~tinderbox/img/*/var/tmp/tb/issues/*/.reported |\
@@ -37,23 +45,31 @@ while read -r f
 do
   buguri=$(cat $f)
   bugno=$(cut -f2 -d'=' <<< $buguri)
-  d=$(dirname $f)
+  d=${f%/*}
   ftitle=$d/title
   image=$(cut -f5 -d'/' <<< $d)
-  cat << EOF >> $index
+  cat << EOF >> $tmpfile
     <tr>
       <td><a href="$buguri">$bugno</a></td>
       <td>$(cat $ftitle)</td>
-      <td><a href="./$image">$image</a></td>
+      <td><a href="./$image/">$image</a></td>
+      <td><a href="./$image/etc/portage/">link</a></td>
+      <td><a href="$(cut -f5- -d'/' <<< $d)/">link</a></td>
     </tr>
 EOF
 
 done
 
-cat << EOF >> $index
+cat << EOF >> $tmpfile
   </tbody>
 </table>
 
 </html>
 
 EOF
+
+if ! diff -q $tmpfile ~tinderbox/img/index.html 1>/dev/null; then
+  cp $tmpfile ~tinderbox/img/index.html
+fi
+
+rm $tmpfile
