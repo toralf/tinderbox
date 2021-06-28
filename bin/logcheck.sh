709370f7ff71d117b777d9afd8acb28a3c66f111
Toralf Förster <toralf.foerster@gmx.de>
2021-06-28 11:26:26 +0200

    bin/logcheck.sh: send out an email b/c crontab does it only at program end

--- bin/logcheck.sh
+++ bin/logcheck.sh
@@ -9,10 +9,14 @@ export LANG=C.utf8
 
 f=/tmp/${0##*/}.out
 
-if [[ ! -s $f && $(wc -c < <(cat ~/logs/*.log 2>/dev/null)) != 0 ]]; then
-  ls -l ~/logs/
-  echo
-  head -v ~/logs/*.log | tee $f
-  echo
-  echo -e "\n\nto re-activate this test again, do:\n\n  tail -v ~/logs/*; rm -f $f;     truncate -s 0 ~/logs/*\n\n"
+if [[ ! -s $f ]]; then
+  if [[ $(wc -c < <(cat ~/logs/*.log 2>/dev/null)) != 0 ]]; then
+    (
+      ls -l ~/logs/
+      echo
+      head -v ~/logs/*.log | tee $f
+      echo
+      echo -e "\n\nto re-activate this test again, do:\n\n  tail -v ~/logs/*; rm -f $f;     truncate -s 0 ~/logs/*\n\n"
+    ) | mail -s "INFO: tinderbox logs" ${MAILTO:-tinderbox}
+  fi
 fi
