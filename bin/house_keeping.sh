58e3198118cc989600d51899aa88cd7ca18156c7
Toralf Förster <toralf.foerster@gmx.de>
2021-07-03 21:52:45 +0200

    bin/house_keeping.sh: improve performance of find + do not delete files, but just dirs in ~/img

--- bin/house_keeping.sh
+++ bin/house_keeping.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 # set -x
 
-
 set -eu
 export LANG=C.utf8
-
 
 if [[ "$(whoami)" != "root" ]]; then
   echo " you must be root"
   exit 1
 fi
 
-find ~tinderbox/distfiles/ -maxdepth 1 -type f -atime +365 -exec rm "{}" \;
+find ~tinderbox/distfiles/ -maxdepth 1 -type f -atime +365 -exec rm {} +
 
-# clean up if either >X% used or <Y GB are free
 fs=/dev/nvme0n1p4
-while [[ -n $(df -m | awk ' $1 == "'"$fs"'" && ($3/$2 > 0.85 || $4 < 200000)') ]]
+while [[ -n $(df -m $fs | awk ' $1 == "'"$fs"'" && ($4 < 200000 || $5 > "85%")') ]]
 do
-  img=$(ls -d ~tinderbox/img/* 2>/dev/null | sort -t'-' -k 3,4 | head -n 1)
+  img=$(ls -d ~tinderbox/img/*/ 2>/dev/null | sort -t'-' -k 3,4 | head -n 1)
   if [[ -d $img ]]; then
     echo
     date
