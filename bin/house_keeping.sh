50695a164bf0b415bfc342d1235188cc909419bb
Toralf Förster <toralf.foerster@gmx.de>
2021-07-04 20:09:05 +0200

    bin/house_keeping.sh: fix corner case

--- bin/house_keeping.sh
+++ bin/house_keeping.sh
@@ -14,14 +14,17 @@ find ~tinderbox/distfiles/ -maxdepth 1 -type f -atime +365 -exec rm {} +
 fs=/dev/nvme0n1p4
 while [[ -n $(df -m $fs | awk ' $1 == "'"$fs"'" && ($4 < 200000 || $5 > "85%")') ]]
 do
+  echo
+  date
+  df -m $fs
   img=$(ls -d ~tinderbox/img/*/ 2>/dev/null | sort -t'-' -k 3,4 | head -n 1)
   if [[ -d $img ]]; then
-    echo
-    date
-    df -m $fs
     echo "prune image: $img"
     rm -rf $img
-    sleep 60              # lazy btrfs
+    sleep 60    # lazy btrfs
     df -m $fs
+  else
+    echo "nothing to delete ?!"
+    break
   fi
 done
