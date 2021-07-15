e4049907767fcf9b75f3ab7a26319466a053f21a
Toralf Förster <toralf.foerster@gmx.de>
2021-07-15 17:47:17 +0200

    bin/cgroup.sh: reserve 3 (formerly: 3.50 vCPU)

--- bin/cgroup.sh
+++ bin/cgroup.sh
@@ -33,8 +33,8 @@ done
 name=/local
 cgcreate -g cpu,memory:$name
 
-# reserve 3.5 vCPUs, 18 GB RAM and 64 GB vRAM for others
-vcpu=${1:-$(($(nproc) * 100000 - 350000))}
+# reserve 3 vCPUs, 18 GB RAM and 64 GB vRAM for others
+vcpu=${1:-$(($(nproc) * 100000 - 300000))}
 ram=${2:-110G}
 vram=${3:-320G}
 cgset -r cpu.cfs_quota_us=$vcpu             $name
