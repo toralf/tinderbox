a2e8b353bdeddeab6b84b1261b8c3d388e19cc8d
Toralf Förster <toralf.foerster@gmx.de>
2021-07-20 15:42:55 +0200

    bin/index.sh: refactor code into funcitons

--- bin/index.sh
+++ bin/index.sh
@@ -4,51 +4,43 @@
 # create ~tinderbox/img/index.html from .reported file in issues directory
 
 
-set -eu
-export LANG=C.utf8
 
+function listImages()  {
+  cat << EOF >> $tmpfile
 
-cat << EOF > ~tinderbox/img/robots.txt
-User-agent: *
-Disallow: /
-
-EOF
-
-tmpfile=$(mktemp /tmp/$(basename $0)_XXXXXX.tmp)
-
-cat << EOF >> $tmpfile
-<html>
-
-<h1>recent <a href="https://zwiebeltoralf.de/tinderbox.html">tinderbox</a> data</h1>
-
-EOF
-
-cat << EOF >> $tmpfile
+<br>
 <h2>content of image directory</h2>
 
-<p><i>image</i>/var/tmp/tb contains ./logs and all ./issues</p>
+<i>image</i>/var/tmp/tb contains ./logs and ./issues
 
 EOF
-ls  ~tinderbox/img/ |\
-while read d
-do
-  cat << EOF >> $tmpfile
+  ls  ~tinderbox/img/ |\
+  while read d
+  do
+    cat << EOF >> $tmpfile
   <a href="./$d">$d</a><br>
 EOF
-done
+  done
 
-cat << EOF >> $tmpfile
+  cat << EOF >> $tmpfile
+
+EOF
+}
+
+
+function listBugs() {
+  cat << EOF >> $tmpfile
 <h2>reported <a href="https://bugs.gentoo.org/">Gentoo Bugs</a></h2>
 
-<table border="0" align="left">
+<table border="0" align="left" class="list_table">
 
   <thead align="left">
     <tr>
       <th>Bug</th>
       <th>Title</th>
       <th>/</th>
-      <th>/etc/portage</th>
-      <th>Issue</th>
+      <th>/etc/portage/</th>
+      <th>IssueDir</th>
     </tr>
   </thead>
 
@@ -63,17 +55,18 @@ cat << EOF >> $tmpfile
   </tfoot>
 
   <tbody>
+
 EOF
 
-ls -t ~tinderbox/img/*/var/tmp/tb/issues/*/.reported |\
-while read -r f
-do
-  buguri=$(cat $f)
-  bugno=$(cut -f2 -d'=' <<< $buguri)
-  d=${f%/*}
-  ftitle=$d/title
-  image=$(cut -f5 -d'/' <<< $d)
-  cat << EOF >> $tmpfile
+  ls -t ~tinderbox/img/*/var/tmp/tb/issues/*/.reported |\
+  while read -r f
+  do
+    buguri=$(cat $f)
+    bugno=$(cut -f2 -d'=' <<< $buguri)
+    d=${f%/*}
+    ftitle=$d/title
+    image=$(cut -f5 -d'/' <<< $d)
+    cat << EOF >> $tmpfile
     <tr>
       <td><a href="$buguri">$bugno</a></td>
       <td>$(recode ascii..html < $ftitle)</td>
@@ -83,20 +76,49 @@ do
     </tr>
 EOF
 
-done
+  done
 
-cat << EOF >> $tmpfile
+  cat << EOF >> $tmpfile
   </tbody>
 </table>
 
 EOF
+}
+
+
+function DisallowRobots() {
+  cat << EOF > ~tinderbox/img/robots.txt
+User-agent: *
+Disallow: /
+
+EOF
+}
+
+
+#######################################################################
+set -eu
+export LANG=C.utf8
+
+tmpfile=$(mktemp /tmp/$(basename $0)_XXXXXX.tmp)
+
+cat << EOF >> $tmpfile
+<html>
+
+<h1>recent <a href="https://zwiebeltoralf.de/tinderbox.html">tinderbox</a> data</h1>
+
+EOF
+
+listBugs
+listImages
 
 cat << EOF >> $tmpfile
 </html>
+
 EOF
 
 if ! diff -q $tmpfile ~tinderbox/img/index.html 1>/dev/null; then
   cp $tmpfile ~tinderbox/img/index.html
+  DisallowRobots
 fi
 
 rm $tmpfile
