4dda67d860d935fd1880191f5ed4e2b15790683f
Toralf Förster <toralf.foerster@gmx.de>
2021-08-11 10:01:16 +0200

    bin/house_keeping.sh: fix race in find-delete

--- bin/house_keeping.sh
+++ bin/house_keeping.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # set -x
 
+
+function doClean()  {
+  local fs=/dev/nvme0n1p4
+  [[ -n $(df -m $fs | awk ' $1 == "'"$fs"'" && ($4 < 200000 || $5 > "89%")') ]]
+}
+
+
+#######################################################################
 set -eu
 export LANG=C.utf8
 
@@ -9,17 +17,19 @@ if [[ "$(whoami)" != "root" ]]; then
   exit 1
 fi
 
-find ~tinderbox/distfiles/ -maxdepth 1 -type f -atime +365 -exec rm {} +
+if doClean; then
+  find ~tinderbox/distfiles/ -maxdepth 1 -type f -mtime +365                 -delete -ignore_readdir_race
+  find ~tinderbox/distfiles/ -maxdepth 1 -type f -mtime +8   -name 'stage3*' -delete -ignore_readdir_race
 
-fs=/dev/nvme0n1p4
-while [[ -n $(df -m $fs | awk ' $1 == "'"$fs"'" && ($4 < 200000 || $5 > "88%")') ]]
-do
-  img=$(ls -d ~tinderbox/img/*/ 2>/dev/null | sort -t'-' -k 3,4 | head -n 1)
-  if [[ -d $img ]]; then
-    rm -r $img || exit $?
-    sleep 30    # lazy btrfs
-  else
-    echo "nothing to delete ?!"
-    exit 1
-  fi
-done
+  while doClean
+  do
+    find ~tinderbox/img/ -mindepth 1 -maxdepth 1 -type d -name '*_*-20??????-??????' |\
+    sort -t'-' -k 3,4 |\
+    head -n 1 |\
+    while read -r img
+    do
+      rm -r $img || exit $?
+      sleep 30    # lazy btrfs
+    done
+  done
+fi
