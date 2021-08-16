46f866e67f30f897ac7cf86ed80e4c243ed3d6de
Toralf Förster <toralf.foerster@gmx.de>
2021-08-16 21:49:55 +0200

    bin/house_keeping.sh: function name spelling

--- bin/house_keeping.sh
+++ bin/house_keeping.sh
@@ -2,7 +2,7 @@
 # set -x
 
 
-function doClean()  {
+function pruneNeeded()  {
   local fs=/dev/nvme0n1p4
   [[ -n $(df -m $fs | awk ' $1 == "'"$fs"'" && ($4 < 200000 || $5 > "89%")') ]]
 }
@@ -17,18 +17,18 @@ if [[ "$(whoami)" != "root" ]]; then
   exit 1
 fi
 
-if doClean; then
+if pruneNeeded; then
   find ~tinderbox/distfiles/ -maxdepth 1 -type f -mtime +365                 -delete -ignore_readdir_race
   find ~tinderbox/distfiles/ -maxdepth 1 -type f -mtime +8   -name 'stage3*' -delete -ignore_readdir_race
 
-  while doClean
+  while pruneNeeded
   do
     find ~tinderbox/img/ -mindepth 1 -maxdepth 1 -type d -name '*_*-20??????-??????' |\
     sort -t'-' -k 3,4 |\
     head -n 1 |\
     while read -r img
     do
-      rm -r $img || exit $?
+      rm -r $img
       sleep 30    # lazy btrfs
     done
   done
