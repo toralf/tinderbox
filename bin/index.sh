06001953a315acca1e10e7e749f15bdae4b9fce7
Toralf Förster <toralf.foerster@gmx.de>
2021-12-10 18:27:04 +0100

    bin/index.sh: quieten recode errors

--- bin/index.sh
+++ bin/index.sh
@@ -8,25 +8,25 @@ function listStat()  {
   echo -e "<h2>few stats</h2>\n<pre>" >> $tmpfile
   date >> $tmpfile
   echo "<h3>coverage</h3>" >> $tmpfile
-  $(dirname $0)/whatsup.sh -c | recode ascii..html >> $tmpfile
-  (cd  ~tinderbox/img; ls packages.*.*covered.txt 2>/dev/null) | recode ascii..html | xargs --no-run-if-empty -I{} echo '<a href="./{}">{}</a>' >> $tmpfile
+  $(dirname $0)/whatsup.sh -c | recode --silent ascii..html >> $tmpfile
+  (cd  ~tinderbox/img; ls packages.*.*covered.txt 2>/dev/null) | recode --silent ascii..html | xargs --no-run-if-empty -I{} echo '<a href="./{}">{}</a>' >> $tmpfile
   echo "<h3>overview</h3>" >> $tmpfile
-  $(dirname $0)/whatsup.sh -o | recode ascii..html >> $tmpfile
+  $(dirname $0)/whatsup.sh -o | recode --silent ascii..html >> $tmpfile
   echo "<h3>packages per day per image</h3>" >> $tmpfile
-  $(dirname $0)/whatsup.sh -d | recode ascii..html >> $tmpfile
+  $(dirname $0)/whatsup.sh -d | recode --silent ascii..html >> $tmpfile
   echo "<h3>packages per hour</h3>" >> $tmpfile
-  $(dirname $0)/whatsup.sh -e | recode ascii..html >> $tmpfile
+  $(dirname $0)/whatsup.sh -e | recode --silent ascii..html >> $tmpfile
   echo "<h3>current task per image</h3>" >> $tmpfile
-  $(dirname $0)/whatsup.sh -t | recode ascii..html >> $tmpfile
+  $(dirname $0)/whatsup.sh -t | recode --silent ascii..html >> $tmpfile
   echo "<h3>current emerge per image</h3>" >> $tmpfile
-  $(dirname $0)/whatsup.sh -l | recode ascii..html >> $tmpfile
+  $(dirname $0)/whatsup.sh -l | recode --silent ascii..html >> $tmpfile
   echo -e "\n</pre>\n" >> $tmpfile
 }
 
 
 function listImages()  {
   echo -e "<h2>content of directory ~tinderbox/img</h2>\nHint: Tinderbox data are under ./var/tmp/tb\n<br>\n<pre>"  >> $tmpfile
-  ls ~tinderbox/img/ | recode ascii..html | xargs --no-run-if-empty -I{} echo '<a href="./{}">{}</a>' >> $tmpfile
+  ls ~tinderbox/img/ | recode --silent ascii..html | xargs --no-run-if-empty -I{} echo '<a href="./{}">{}</a>' >> $tmpfile
   echo -e "</pre>\n" >> $tmpfile
 }
 
@@ -72,7 +72,7 @@ EOF
     cat << EOF >> $tmpfile
     <tr>
       <td><a href="$buguri">$bugno</a></td>
-      <td>$(recode ascii..html < $ftitle)</td>
+      <td>$(recode --silent ascii..html < $ftitle)</td>
       <td><a href="./$image/">$image</a></td>
       <td><a href="./$image/etc/portage/">link</a></td>
       <td><a href="$(cut -f5- -d'/' <<< $d)/">link</a></td>
