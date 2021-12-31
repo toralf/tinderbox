b262de823a597d02f8c3fd025108189957ffe793
Toralf Förster <toralf.foerster@gmx.de>
2021-12-31 10:33:38 +0100

    bin/house_keeping.sh: remove broken setup images first

--- bin/house_keeping.sh
+++ bin/house_keeping.sh
@@ -2,7 +2,7 @@
 # set -x
 
 
-function sortCandidatesByAge()  {
+function getCandidates()  {
   find ~tinderbox/img/ -mindepth 1 -maxdepth 1 -type d -name '*-j*-20??????-??????' |\
   while read -r i
   do
@@ -14,9 +14,7 @@ function sortCandidatesByAge()  {
       continue
     fi
 
-    local starttime=$(getStartTime $i 2>/dev/null || stat -c%Y $i)
-    local full_days=$(echo "scale=0; ( $(date +%s) - $starttime ) / 86400" | bc)
-    if [[ $full_days -lt 1 ]]; then
+    if [[ $(( $(date +%s) - $(stat -c %Y $i) )) -lt 86400 ]]; then
       continue
     fi
 
@@ -33,6 +31,7 @@ function pruneNeeded()  {
   local fs=/dev/nvme0n1p4
   local gb=200000          # wanted free space in GB
   local perc=89            # wanted free space in percent
+
   [[ -n $(df -m $fs | awk ' $1 == "'"$fs"'" && ($4 < "'"$gb"'" || $5 > "'"$perc"'%")') ]]
 }
 
@@ -42,7 +41,8 @@ function pruneDir() {
   local d=$1
 
   if [[ ! -d $d ]]; then
-    return 1
+    echo "$d is not a dir !"
+    exit 1
   fi
 
   # https://forums.gentoo.org/viewtopic-p-6072905.html?sid=461188c03d3c4d08de80136a49982d86#6072905
@@ -69,8 +69,8 @@ fi
 source $(dirname $0)/lib.sh
 
 if pruneNeeded; then
-  # prune distfiles older than 1 yr
-  find ~tinderbox/distfiles/ -maxdepth 1 -type f -mtime +365 -delete
+  # prune distfiles not accessed within 1 yr
+  find ~tinderbox/distfiles/ -maxdepth 1 -type f -atime +365 -delete
 
   # prune stage3 files
   latest=~tinderbox/distfiles/latest-stage3.txt
@@ -78,23 +78,33 @@ if pruneNeeded; then
     find ~tinderbox/distfiles/ -maxdepth 1 -type f -name 'stage3-amd64-*.xz' |\
     while read -r stage3
     do
-      if [[ $latest -nt $stage3 ]] && ! grep -q $(basename $stage3) $latest; then
-        rm ${stage3}*
+      if [[ $latest -nt $stage3 ]]; then
+        if ! grep -q $(basename $stage3) $latest; then
+          rm ${stage3}*
+        fi
       fi
     done
   fi
-
-  # prune images w/o any reported bug
-  while read -r img && pruneNeeded
-  do
-    if ! ls $img/var/tmp/tb/issues/*/.reported &>/dev/null; then
-      pruneDir $img
-    fi
-  done < <(sortCandidatesByAge)
-
-  # prune images from oldest to newer
-  while read -r img && pruneNeeded
-  do
-    pruneDir $img
-  done < <(sortCandidatesByAge)
 fi
+
+# prune images with incompleted setup
+while read -r img && pruneNeeded
+do
+  if [[ ! -f $img/var/log/emerge.log || ! -d $img/var/tmp/tb ]]; then
+    pruneDir $img
+  fi
+done < <(getCandidates)
+
+# prune images w/o any reported bug
+while read -r img && pruneNeeded
+do
+  if ! ls $img/var/tmp/tb/issues/*/.reported &>/dev/null; then
+    pruneDir $img
+  fi
+done < <(getCandidates)
+
+# prune images from oldest to newer
+while read -r img && pruneNeeded
+do
+  pruneDir $img
+done < <(getCandidates)
