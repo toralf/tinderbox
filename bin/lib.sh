e33e139f884f5800805b9f62ba068b9522ae092a
Toralf Förster <toralf.foerster@gmx.de>
2022-02-16 21:26:29 +0100

    bin/lib.sh: new __is_cgrouped()

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,9 +1,13 @@
 function __is_running() {
-  local b=$(basename $1)
-
-  [[ -d /sys/fs/cgroup/cpu/local/$b/ ]] || __is_locked $1
+  __is_cgrouped $1 || __is_locked $1
 }
 
+
+function __is_cgrouped() {
+  local b=$(basename $1)
+
+  [[ -d /sys/fs/cgroup/cpu/local/$b/ ]]
+}
 
 function __is_locked() {
   local b=$(basename $1)
@@ -17,4 +21,3 @@ function __getStartTime() {
 
   cat ~tinderbox/img/$b/var/tmp/tb/setup.timestamp
 }
-
