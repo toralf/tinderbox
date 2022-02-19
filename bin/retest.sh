toralf/tinderbox

commit b879948152c02aeb834d637a4bee3dd1e442b6d9

bin/retest.sh: now @set are accepted

bin/retest.sh
@@ -15,23 +15,21 @@ source $(dirname $0)/lib.sh
 result=/tmp/${0##*/}.txt  # package/s for the appropriate backlog
 truncate -s 0 $result
 
-grep -e '@' -e '%' -e '=' <<< ${@} >> $result || true
-
-grep -v -e '@' -e '%' -e '=' <<< ${@} |\
-xargs -n 1 |\
+grep    -e '^@' -e '^%' -e '^='        <<< ${@} >> $result || true
+grep -v -e '^@' -e '^%' -e '^=' -e '#' <<< ${@} |\
+xargs --no-run-if-empty -n 1 |\
 sort -u |\
 while read -r atom
 do
   echo "$atom" >> $result
-
-  # delete a package in global and image specific files
-  pkgname=$(qatom -F "%{CATEGORY}/%{PN}" "$atom" 2>/dev/null | grep -v -F '<unset>')
+  # delete from global and image specific files
+  pkgname=$(qatom -F "%{CATEGORY}/%{PN}" "$atom" 2>/dev/null | grep -v -F '<unset>' | sed -e 's,/,\\/,g')
   if [[ -n "$pkgname" ]]; then
-    if ! sed -i -e "/$(sed -e 's,/,\\/,' <<< $pkgname)/d"  \
-        ~/tb/data/ALREADY_CATCHED                     \
-        ~/run/*/etc/portage/package.mask/self         \
+    if ! sed -i -e "/$pkgname/d" \
+        ~/tb/data/ALREADY_CATCHED \
+        ~/run/*/etc/portage/package.mask/self \
         ~/run/*/etc/portage/package.env/{cflags_default,nosandbox,test-fail-continue} 2>/dev/null; then
-      # ^^ not all files might exist in current ~/run
+      # ^^ those files might not exist currently
       :
     fi
   fi
@@ -45,7 +43,7 @@ if [[ -s $result ]]; then
     (sort -u $result | grep -v -F -f $bl | shuf; cat $bl) > $bl.tmp
     mv $bl.tmp $bl
 
-    # force a repo sync before re-test
+    # force repo sync before all re-test, but once is enough
     sed -i -e '/%syncRepos/d' $bl
     echo "%syncRepos" >> $bl
   done

commit cb5ae090ce54622025d3d273dbcdf95a42dfc842

bin/retest.sh: fix a bug introduced in b879948152c02ae

bin/retest.sh
@@ -3,7 +3,7 @@
 
 
 # call this eg by:
-# grep 'setup phase' ~/tb/data/ALREADY_CATCHED | sed -e 's,\[.*\] ,,g' | cut -f1 -d' ' -s | xargs -r qatom -F "%{CATEGORY}/%{PN}" | xargs retest.sh
+# grep 'setup phase' ~/tb/data/ALREADY_CATCHED | sed -e 's,\[.*\] ,,g' | cut -f1 -d' ' | xargs retest.sh
 
 
 set -eu
@@ -29,9 +29,10 @@ sort -u |\
 while read -r item
 do
   echo "$item" >> $result
-  pkgname=$(qatom -F "%{CATEGORY}/%{PN}" "$item" 2>/dev/null | grep -v -F '<unset>' | sed -e 's,/,\\/,g')
-  if [[ -n "$pkgname" ]]; then
-    if ! sed -i -e "/$pkgname/d" \
+  # reset issue artefacts
+  pkgname=$(qatom -F "%{CATEGORY}/%{PN}" "$item" 2>/dev/null | grep -v -F '<unset>' | grep ".*/.*")
+  if [[ -n $pkgname ]]; then
+    if ! sed -i -e "/$(sed -e 's,/,\\/,' <<< $pkgname)-[[:digit:]]/d" \
         ~tinderbox/tb/data/ALREADY_CATCHED \
         ~tinderbox/run/*/etc/portage/package.mask/self \
         ~tinderbox/run/*/etc/portage/package.env/{cflags_default,nosandbox,test-fail-continue} 2>/dev/null; then
@@ -41,11 +42,12 @@ do
 done
 
 if [[ -s $result ]]; then
-  for i in $(ls ~tinderbox/run 2>/dev/null)
+  for bl in $(ls ~tinderbox/run/*/var/tmp/tb/backlog.1st 2>/dev/null)
   do
-    bl=~tinderbox/run/$i/var/tmp/tb/backlog.1st
+    tmp=$(mktemp /tmp/bl_XXXXXX)
     # filter out dups, then put new entries after existing ones
-    (sort -u $result | grep -v -F -f $bl | shuf; cat $bl) > $bl.tmp
-    mv $bl.tmp $bl
+    (sort -u $result | grep -v -F -f $bl | shuf; cat $bl) > $tmp
+    cp $tmp $bl
+    rm $tmp
   done
 fi