9b04526474456a7424f2652ffcb4bc576ac40b9f
Toralf Förster <toralf.foerster@gmx.de>
2022-06-28 16:55:09 +0200

    shuffle code around

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,5 +1,7 @@
-function __is_running() {
-  __is_cgrouped $1 || __is_locked $1
+function __getStartTime() {
+  local b=$(basename $1)
+
+  cat ~tinderbox/img/$b/var/tmp/tb/setup.timestamp
 }
 
 
@@ -9,6 +11,7 @@ function __is_cgrouped() {
   [[ -d /sys/fs/cgroup/cpu/local/$b/ ]]
 }
 
+
 function __is_locked() {
   local b=$(basename $1)
 
@@ -16,8 +19,6 @@ function __is_locked() {
 }
 
 
-function __getStartTime() {
-  local b=$(basename $1)
-
-  cat ~tinderbox/img/$b/var/tmp/tb/setup.timestamp
+function __is_running() {
+  __is_cgrouped $1 || __is_locked $1
 }
