93efd949430ab29ceedadb536dd1689051264f88
Toralf Förster <toralf.foerster@gmx.de>
2022-10-04 11:26:05 +0200

    kick off developer info and image listing

--- bin/index.sh
+++ bin/index.sh
@@ -2,39 +2,41 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-# create ~tinderbox/img/index.html
+# create the index file ~tinderbox/img/index.html
 
 
 function listStat()  {
   date >> $tmpfile
-  echo -e "<h2>few stats</h2>\n<pre>" >> $tmpfile
+  echo "<h2>few stats</h2>" >> $tmpfile
+  echo "<pre>" >> $tmpfile
   echo "<h3>coverage</h3>" >> $tmpfile
   $(dirname $0)/whatsup.sh -c | recode --silent ascii..html >> $tmpfile
-  (cd  ~tinderbox/img; ls | grep -v '17.') | recode --silent ascii..html | xargs --no-run-if-empty -I{} echo '<a href="./{}">{}</a>' >> $tmpfile
   echo "<h3>overview</h3>" >> $tmpfile
   $(dirname $0)/whatsup.sh -o | recode --silent ascii..html >> $tmpfile
-  echo "<h3>packages per day per image</h3>" >> $tmpfile
+  echo "<h3>packages per day</h3>" >> $tmpfile
   $(dirname $0)/whatsup.sh -d | recode --silent ascii..html >> $tmpfile
   echo "<h3>packages per hour</h3>" >> $tmpfile
   $(dirname $0)/whatsup.sh -e | recode --silent ascii..html >> $tmpfile
-  echo "<h3>current task per image</h3>" >> $tmpfile
+  echo "<h3>current task</h3>" >> $tmpfile
   $(dirname $0)/whatsup.sh -t | recode --silent ascii..html >> $tmpfile
-  echo "<h3>current emerge per image</h3>" >> $tmpfile
+  echo "<h3>current package</h3>" >> $tmpfile
   $(dirname $0)/whatsup.sh -l | recode --silent ascii..html >> $tmpfile
   echo -e "\n</pre>\n" >> $tmpfile
 }
 
 
-function listImages()  {
-  echo -e "<h2>content of directory ~tinderbox/img</h2>\nHint: Tinderbox data are under ./var/tmp/tb\n<br>\n<pre>"  >> $tmpfile
-  (cd ~tinderbox/img; ls -d 17.* 2>/dev/null) | recode --silent ascii..html | xargs --no-run-if-empty -I{} echo '<a href="./{}">{}</a>' >> $tmpfile
-  echo -e "</pre>\n" >> $tmpfile
+function listFiles()  {
+  echo "<h2>downloadable files</h2>" >> $tmpfile
+  echo "<pre>" >> $tmpfile
+  (cd ~tinderbox/img; find . -maxdepth 1 -type f) | recode --silent ascii..html |
+  xargs --no-run-if-empty -I{} echo '<a href="./{}">{}</a>' >> $tmpfile
+  echo -e "\n</pre>\n" >> $tmpfile
 }
 
 
 function listBugs() {
   cat << EOF >> $tmpfile
-<h2>reported <a href="https://bugs.gentoo.org/">Gentoo Bugs</a> of images in ~tinderbox/img </h2>
+<h2><a href="https://bugs.gentoo.org/">Gentoo Bugs</a> and links to inspect the image content</h2>
 
 <table border="0" align="left" class="list_table">
 
@@ -62,7 +64,7 @@ function listBugs() {
 
 EOF
 
-  ls -t ~tinderbox/img/*/var/tmp/tb/issues/*/.reported 2>/dev/null |
+  ls -t -- ~tinderbox/img/*/var/tmp/tb/issues/*/.reported 2>/dev/null |
   while read -r f
   do
     uri=$(cat $f 2>/dev/null) || continue    # race with house keeping
@@ -86,20 +88,6 @@ EOF
 }
 
 
-function developerInfo() {
-  # from _sam (libxcrypt)
-  cd ~tinderbox/img
-
-  find ./*/var/db/pkg/ -name "NEEDED.ELF.2" -not -path '*/-MERGING*' -exec grep -F "libcrypt.so" {} + 2>/dev/null |
-  sort -u |
-  tee /tmp/needed.txt |
-  cut -f6- -d'/' -s |
-  sort -u > /tmp/needed-short.txt
-
-  cp /tmp/needed*.txt ./
-}
-
-
 #######################################################################
 set -eu
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin:/opt/tb/bin"
@@ -119,11 +107,9 @@ cat << EOF >> $tmpfile
 
 EOF
 listStat
-listImages
+listFiles
 listBugs
 echo -e "\n</html>\n" >> $tmpfile
 
 cp $tmpfile ~tinderbox/img/index.html
 rm $tmpfile
-
-developerInfo
