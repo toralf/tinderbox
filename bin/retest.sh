314445e4780c3dd7ec40c8c581a997881efd7b00
Toralf Förster <toralf.foerster@gmx.de>
2023-04-21 18:50:39 +0200

    always put into 1st prio backlog

--- bin/retest.sh
+++ bin/retest.sh
@@ -16,18 +16,12 @@ fi
 
 result=/tmp/$(basename $0) # package/s to be scheduled in the backlog of each image
 
-first=0
-if [[ $1 == "1st" ]]; then
-  first=1
-  shift
-fi
-
-# accept 1st *lines* w/o any check
-grep -e '^@' -e '^%' -e '^=' <<<${@} |
+# accept special atoms w/o any check
+grep -e '^@' -e '^%' -e '^=' <<<$* |
   sort -u >$result.1st
 
 # work at regular atoms
-grep -v -e '^@' -e '^%' -e '^=' -e '#' <<<${@} |
+grep -v -e '^@' -e '^%' -e '^=' -e '#' <<<$* |
   sort -u |
   xargs qatom -F "%{CATEGORY}/%{PN}" 2>/dev/null |
   grep -v -F '<unset>' |
@@ -43,22 +37,14 @@ grep -v -e '^@' -e '^%' -e '^=' -e '#' <<<${@} |
 
 if [[ -s $result ]]; then
   tmp=$(mktemp /tmp/retest.sh_XXXXXX)
-  if [[ $first -eq 1 ]]; then
-    for bl in $(ls ~tinderbox/run/*/var/tmp/tb/backlog.1st 2>/dev/null); do
-      # put new entries shuffled after existing ones, filter out dups before
-      (
-        grep -v -F -f $bl $result | shuf
-        cat $bl
-      ) >$tmp
-      cp $tmp $bl
-    done
-  else
-    for bl in $(ls ~tinderbox/run/*/var/tmp/tb/backlog.upd 2>/dev/null); do
-      # mix existing and new entries, resolve dups
-      cat $bl $result | sort -u | shuf >$tmp
-      cp $tmp $bl
-    done
-  fi
+  for bl in $(ls ~tinderbox/run/*/var/tmp/tb/backlog.1st 2>/dev/null); do
+    # put new entries shuffled after existing ones, filter out dups before
+    (
+      grep -v -F -f $bl $result | shuf
+      cat $bl
+    ) >$tmp
+    cp $tmp $bl
+  done
   rm $tmp
 fi
 
