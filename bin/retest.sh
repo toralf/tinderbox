56479e1bf2a5a99d93453dadf73a22e74e4a8add
Toralf Förster <toralf.foerster@gmx.de>
2023-04-21 22:03:06 +0200

    fix fallout from previous commits

--- bin/retest.sh
+++ bin/retest.sh
@@ -17,43 +17,51 @@ fi
 result=/tmp/$(basename $0) # package/s to be scheduled in the backlog of each image
 
 # accept special atoms w/o any check
-grep -e '^@' -e '^%' -e '^=' <<<$* |
-  xargs -n 1 |
-  sort -u >$result.1st
+xargs -n 1 <<<$* |
+  grep -e '^@' -e '^%' -e '^=' |
+  sort -u >$result.special
 
 # work at regular atoms
-grep -v -e '^@' -e '^%' -e '^=' -e '#' <<<$* |
+xargs -n 1 <<<$* |
+  grep -v -e '^@' -e '^%' -e '^=' -e '#' |
   xargs -n 1 |
   sort -u |
   xargs qatom -F "%{CATEGORY}/%{PN}" 2>/dev/null |
   grep -v -F '<unset>' |
   grep ".*/.*" |
-  sort -u |
-  tee $result |
+  sort -u >$result
+
+if [[ -s $result ]]; then
+  # emerge new atoms shuffled after existings
+  tmp=$(mktemp /tmp/retest.sh_XXXXXX)
+  for bl in $(ls ~tinderbox/run/*/var/tmp/tb/backlog.1st 2>/dev/null); do
+    (
+      # grep out dups
+      grep -v -F -f $bl $result | shuf
+      cat $bl
+    ) >$tmp
+    uniq $tmp >$bl
+  done
+  rm $tmp
+
+  # reset atom in image specific files
   while read -r pkgname; do
     sed -i -e "/$(sed -e 's,/,\\/,' <<<$pkgname)\-[[:digit:]]/d" \
       ~tinderbox/tb/data/ALREADY_CAUGHT \
       ~tinderbox/run/*/etc/portage/package.mask/self \
       ~tinderbox/run/*/etc/portage/package.env/{cflags_default,nosandbox,test-fail-continue} 2>/dev/null || true
-  done
+  done <$result
+fi
 
-if [[ -s $result ]]; then
+if [[ -s $result.special ]]; then
+  # emerge special entries before existings
   tmp=$(mktemp /tmp/retest.sh_XXXXXX)
   for bl in $(ls ~tinderbox/run/*/var/tmp/tb/backlog.1st 2>/dev/null); do
-    # put new entries shuffled after existing ones, filter out dups before
-    (
-      grep -v -F -f $bl $result | shuf
-      cat $bl
-    ) >$tmp
-    cp $tmp $bl
+    cp $bl $tmp
+    shuf $result.special >>$tmp
+    uniq $tmp >$bl
   done
   rm $tmp
 fi
 
-if [[ -s $result.1st ]]; then
-  for bl in $(ls ~tinderbox/run/*/var/tmp/tb/backlog.1st 2>/dev/null); do
-    shuf $result.1st >>$bl
-  done
-fi
-
-rm $result{,.1st}
+rm $result{,.special}
