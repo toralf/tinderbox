da6220fbcc1be9bb2a6d3751a3abd3a1e8384c46
Toralf Förster <toralf.foerster@gmx.de>
2023-08-20 12:52:29 +0200

    fix growing of needed*.txt

--- bin/collect_data.sh
+++ bin/collect_data.sh
@@ -12,10 +12,6 @@ if [[ "$(whoami)" != "root" ]]; then
   exit 1
 fi
 
-if ls /tmp/$(basename $0)_* 2>/dev/null; then
-  echo "another instance seems to run" >&2
-  exit 1
-fi
 tmpfile=$(mktemp /tmp/$(basename $0)_XXXXXX.tmp)
 
 # sam_
@@ -28,37 +24,35 @@ if sort -u ~tinderbox/img/*/var/tmp/xgqt.txt >$tmpfile 2>/dev/null; then
   sort -nr $tmpfile >~tinderbox/img/xgqt.txt
 fi
 
-# sam_ + flow
-if [[ ${1-} == "reset" ]]; then
-  # run this monthly or so to get rid of old stuff
-  scope=""
-  since=""
-else
-  scope="run"
-  since="-cmin -65" # job runs hourly for <1min, so a 5 min overlap seems sufficient
-fi
-
-if [[ ${1-} == "reset" ]]; then
-  truncate -s 0 $tmpfile
-else
-  cp ~tinderbox/img/needed.ELF.2.txt $tmpfile
-fi
-find ~tinderbox/${scope:-img}/*/var/db/pkg/ -mindepth 3 -maxdepth 4 -name "NEEDED.ELF.2" ${since-} 2>/dev/null |
-  grep -v -F '/-MERGING-' |
-  xargs -r cat 2>/dev/null |
-  sort -u >>$tmpfile
+# sam_ + flow, run "reset" from time to time to clean up, otherwise run this hourly (65 == 5 min overlap)
+(
+  if [[ ${1-} == "reset" ]]; then
+    find ~tinderbox/img/*/var/db/pkg/ -mindepth 3 -maxdepth 4 -name "NEEDED.ELF.2" 2>/dev/null |
+      grep -v -F '/-MERGING-' |
+      xargs -r cat 2>/dev/null
+  else
+    cat ~tinderbox/img/needed.ELF.2.txt
+    find ~tinderbox/run/*/var/db/pkg/ -mindepth 3 -maxdepth 4 -name "NEEDED.ELF.2" -cmin -65 2>/dev/null |
+      grep -v -F '/-MERGING-' |
+      xargs -r cat 2>/dev/null
+  fi
+) | sort -u >$tmpfile
 cp $tmpfile ~tinderbox/img/needed.ELF.2.txt
 
-if [[ ${1-} == "reset" ]]; then
-  truncate -s 0 $tmpfile
-else
-  cp ~tinderbox/img/needed.txt $tmpfile
-fi
-find ~tinderbox/${scope:-img}/*/var/db/pkg/ -mindepth 3 -maxdepth 4 -name "NEEDED" ${since-} 2>/dev/null |
-  grep -v -F '/-MERGING-' |
-  xargs -r grep -H . 2>/dev/null |
-  sed -e 's,^/home/tinderbox/.*/.*/var/db/pkg/,,' -e 's,/NEEDED:, ,' |
-  sort -u >>$tmpfile
+(
+  if [[ ${1-} == "reset" ]]; then
+    find ~tinderbox/img/*/var/db/pkg/ -mindepth 3 -maxdepth 4 -name "NEEDED" 2>/dev/null |
+      grep -v -F '/-MERGING-' |
+      xargs -r grep -H . 2>/dev/null |
+      sed -e 's,^/home/tinderbox/.*/.*/var/db/pkg/,,' -e 's,/NEEDED:, ,'
+  else
+    cat ~tinderbox/img/needed.txt
+    find ~tinderbox/run/*/var/db/pkg/ -mindepth 3 -maxdepth 4 -name "NEEDED" -cmin -65 2>/dev/null |
+      grep -v -F '/-MERGING-' |
+      xargs -r grep -H . 2>/dev/null |
+      sed -e 's,^/home/tinderbox/.*/.*/var/db/pkg/,,' -e 's,/NEEDED:, ,'
+  fi
+) | sort -u >$tmpfile
 cp $tmpfile ~tinderbox/img/needed.txt
 
 rm $tmpfile
