6f3bff8421203d573824079a637e3942ec658509
Toralf Förster <toralf.foerster@gmx.de>
2023-10-08 20:20:04 +0200

    reap crashed images after 1 day

--- bin/replace_img.sh
+++ bin/replace_img.sh
@@ -77,6 +77,15 @@ while :; do
   done < <(ImagesInRunShuffled)
 
   while read -r oldimg; do
+    if [[ ! -f ~tinderbox/run/$oldimg/var/tmp/tb/EOL ]] && __is_crashed $oldimg; then
+      hours=$(((EPOCHSECONDS - $(stat -c %Z ~tinderbox/img/$oldimg/var/tmp/tb/task)) / 3600))
+      if [[ $hours -ge 24 ]]; then
+        echo -e "image is crashed and last task was $hours hours ago" >>~tinderbox/img/$oldimg/var/tmp/tb/EOL
+      fi
+    fi
+  done < <(ImagesInRunShuffled)
+
+  while read -r oldimg; do
     if __is_running $oldimg; then
       hours=$(((EPOCHSECONDS - $(stat -c %Z ~tinderbox/img/$oldimg/var/tmp/tb/task)) / 3600))
       if [[ $hours -ge 25 ]]; then
