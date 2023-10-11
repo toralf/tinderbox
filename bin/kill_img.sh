3a8c77fd4033cb38ab19eb3132020aff55bef0f7
Toralf Förster <toralf.foerster@gmx.de>
2023-10-11 18:36:46 +0200

    overhaul to avoid kill -9 unconditionally

--- bin/kill_img.sh
+++ bin/kill_img.sh
@@ -2,8 +2,29 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-# set an image EOL, kill a running emerge process -or- the entrypoint script itself
+# kill a running emerge process -or- the entrypoint script itself and set an image EOL
 
+function killPid() {
+  local pid=$1
+
+  pstree -UlnspuTa $pid | head -n 20 | cut -c 1-200 | tee ~tinderbox/img/$img/var/tmp/tb/killed_process.log
+  echo
+  echo -n " stopping $pid "
+  kill -15 $pid
+  i=60
+  while ((i--)) && kill -0 $pid 2>/dev/null; do
+    echo -n '.'
+    sleep 1
+  done
+  echo
+  if kill -0 $pid 2>/dev/null; then
+    echo " notice: get roughly for $pid"
+    kill -9 $pid
+    echo
+  fi
+}
+
+#######################################################################
 set -euf
 export LANG=C.utf8
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin:/opt/tb/bin"
@@ -13,53 +34,46 @@ if [[ "$(whoami)" != "root" ]]; then
   exit 1
 fi
 
-for img in ${*?got no image}; do
-  img=$(basename "$img")
-  if [[ -d ~tinderbox/img/$img ]]; then
-    echo "user decision at $(date)" >>~tinderbox/img/$img/var/tmp/tb/EOL
-    chmod g+w ~tinderbox/img/$img/var/tmp/tb/EOL
-    chgrp tinderbox ~tinderbox/img/$img/var/tmp/tb/EOL
-    if pid_bwrap=$(pgrep -f -u 0 -U 0 -G 0 " $(dirname $0)/bwrap.sh .*$(tr '+' '.' <<<$img)"); then
-      if [[ -n $pid_bwrap && $(wc -l <<<$pid_bwrap) -eq 1 ]]; then
-        if pid_emerge=$(pstree -pa $pid_bwrap | grep -F 'emerge,' | grep -m 1 -Eo ',([[:digit:]]+) ' | tr -d ','); then
-          if [[ -n $pid_emerge ]]; then
-            pstree -UlnspuTa $pid_emerge | head -n 20 | cut -c 1-200 | tee ~tinderbox/img/$img/var/tmp/tb/killed_process.log
-            echo
-            kill -9 $pid_emerge
+img=$(basename $1)
+if [[ -z $img || ! -d ~tinderbox/img/$img ]]; then
+  echo " error: $img: image not found" >&2
+fi
+
+echo "user decision at $(date)" >>~tinderbox/img/$img/var/tmp/tb/EOL
+chmod g+w ~tinderbox/img/$img/var/tmp/tb/EOL
+chgrp tinderbox ~tinderbox/img/$img/var/tmp/tb/EOL
+
+if pid_bwrap=$(pgrep -f -u 0 -U 0 -G 0 " $(dirname $0)/bwrap.sh .*$(tr '+' '.' <<<$img)"); then
+  if [[ -n $pid_bwrap && $(wc -l <<<$pid_bwrap) -eq 1 ]]; then
+    if pid_emerge=$(
+      set -o pipefail
+      pstree -pa $pid_bwrap | grep -F 'emerge,' | grep -m 1 -Eo ',([[:digit:]]+) ' | tr -d ','
+    ); then
+      if [[ -n $pid_emerge ]]; then
+        echo " kill emerge"
+        killPid $pid_emerge
+      else
+        echo " notice: empty emerge pid from $pid_bwrap"
+        if pid_entrypoint=$(
+          set -o pipefail
+          pstree -pa $pid_bwrap | grep -F 'entrypoint,' | grep -m 1 -Eo ',([[:digit:]]+) ' | tr -d ','
+        ); then
+          if [[ -n $pid_entrypoint ]]; then
+            echo " kill entrypoint"
+            killPid $pid_entrypoint
           else
-            echo " notice: empty emerge pid from $pid_bwrap"
-            if pid_entrypoint=$(pstree -pa $pid_bwrap | grep -F 'entrypoint,' | grep -m 1 -Eo ',([[:digit:]]+) ' | tr -d ','); then
-              if [[ -n $pid_entrypoint ]]; then
-                pstree -UlnspuTa $pid_entrypoint | head -n 20 | cut -c1-200
-                echo
-                kill -15 $pid_entrypoint
-                i=60
-                while ((i--)) && kill -0 $pid_entrypoint 2>/dev/null; do
-                  sleep 1
-                done
-                echo
-                if kill -0 $pid_entrypoint 2>/dev/null; then
-                  echo " notice: get roughly for $pid_entrypoint"
-                  kill -9 $pid_entrypoint
-                  echo
-                fi
-              else
-                echo " notice: empty entrypoint pid from $pid_bwrap"
-              fi
-            else
-              echo " notice: could not get entrypoint pid from $pid_bwrap"
-            fi
+            echo " notice: empty entrypoint pid from $pid_bwrap"
           fi
         else
-          echo " notice: could not get emerge pid from $pid_bwrap"
+          echo " notice: could not get entrypoint pid from $pid_bwrap"
         fi
-      else
-        echo " notice: empty bwrap pid"
       fi
     else
-      echo " info: could not get bwrap pid"
+      echo " notice: could not get emerge pid from $pid_bwrap"
     fi
   else
-    echo " error: $img: image not found" >&2
+    echo " notice: empty or non.unique bwrap pid"
   fi
-done
+else
+  echo " info: could not query bwrap pid"
+fi
