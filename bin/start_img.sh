7b131bed0b647e62888c6721b1166040ba0f4cf5
Toralf Förster <toralf.foerster@gmx.de>
2023-10-14 12:36:50 +0200

    fix c43e9a99

--- bin/start_img.sh
+++ bin/start_img.sh
@@ -33,7 +33,7 @@ for i in ${@:-$(ls ~tinderbox/run)}; do
   fi
 
   if [[ -f $mnt/var/tmp/tb/STOP ]]; then
-    if ! __is_not_running "$mnt"; then
+    if __is_not_running "$mnt"; then
       echo " found STOP file for stopped image: $mnt" >&2
     fi
     continue
