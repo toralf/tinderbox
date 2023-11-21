23e85c8b8e5f29b5da39b9f295f06e6a9dc16086
Toralf Förster <toralf.foerster@gmx.de>
2023-11-21 20:14:33 +0100

    13 parallel jobs seems to fit the current work load better

--- bin/replace_img.sh
+++ bin/replace_img.sh
@@ -63,7 +63,7 @@ fi
 echo $$ >"$lockfile"
 trap Finish INT QUIT TERM EXIT
 
-desired_count=${1:-12}
+desired_count=${1:-13}
 while :; do
   while read -r oldimg; do
     if [[ ! -f ~tinderbox/run/$oldimg/var/tmp/tb/EOL ]] && __is_stopped $oldimg; then
