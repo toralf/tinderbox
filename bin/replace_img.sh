69cbe1161202c73956d4acc09ec6a0b1930bd2d0
Toralf Förster <toralf.foerster@gmx.de>
2024-10-19 17:28:15 +0200

    allow parallel run

--- bin/replace_img.sh
+++ bin/replace_img.sh
@@ -13,28 +13,23 @@ function Finish() {
   if [[ $rc -ne 0 ]]; then
     echo " pid $pid exited with rc=$rc"
   fi
-  rm $lockfile
   exit $rc
 }
 
-function ImagesInRunShuffled() {
+function ImagesInRun() {
   (
     set +f
     cd ~tinderbox/run
-    ls -d * 2>/dev/null | shuf
-  )
-}
-
-function ImagesInRunButEOL() {
-  (
-    set +f
-    cd ~tinderbox/run
-    ls */var/tmp/tb/EOL 2>/dev/null | cut -f 1 -d '/' -s
+    ls -d * 2>/dev/null
   )
 }
 
 function FreeSlotAvailable() {
-  [[ $(ls /run/tb | wc -l) -lt $desired_count && $(wc -l < <(ImagesInRunShuffled)) -lt $desired_count ]]
+  local i=$(wc -l < <(ImagesInRun))
+  local r=$(ls /run/tb | wc -l)
+  local s=$(pgrep -fc "/bin/bash /opt/tb/bin/replace")
+
+  [[ $i -lt $desired_count && $((r + s)) -le $desired_count ]]
 }
 
 #######################################################################
@@ -52,8 +47,17 @@ if [[ ! -d /run/tb ]]; then
   exit 1
 fi
 
-source $(dirname $0)/lib.sh
+if [[ $# -eq 1 ]]; then
+  desired_count=$1
+else
+  case $(nproc) in
+  32) desired_count=12 ;;
+  96) desired_count=16 ;;
+  *) desired_count=$(($(nproc) / 3)) ;;
+  esac
+fi
 
+# chain the cleanup phase
 lockfile="/tmp/$(basename $0).lock"
 if [[ -s $lockfile ]]; then
   pid=$(cat $lockfile)
@@ -66,78 +70,67 @@ fi
 echo $$ >"$lockfile"
 trap Finish INT QUIT TERM EXIT
 
-if [[ $# -eq 1 ]]; then
-  desired_count=$1
+source $(dirname $0)/lib.sh
+
+# reap non-responding images
+while read -r oldimg; do
+  if [[ ! -f ~tinderbox/run/$oldimg/var/tmp/tb/EOL ]]; then
+    if [[ -f ~tinderbox/img/$oldimg/var/tmp/tb/task.log ]]; then
+      hours=$(((EPOCHSECONDS - $(stat -c %Z ~tinderbox/img/$oldimg/var/tmp/tb/task.log)) / 3600))
+    elif [[ -f ~tinderbox/img/$oldimg/var/tmp/tb/task ]]; then
+      hours=$(((EPOCHSECONDS - $(stat -c %Z ~tinderbox/img/$oldimg/var/tmp/tb/task)) / 3600))
+    else
+      hours=$(((EPOCHSECONDS - $(getStartTime $oldimg)) / 3600))
+    fi
+
+    if [[ $hours -ge 24 ]]; then
+      if __is_crashed $oldimg; then
+        echo -e "$(basename $0): image crashed $hours hours ago" >>~tinderbox/img/$oldimg/var/tmp/tb/EOL
+      elif __is_stopped $oldimg; then
+        echo -e "$(basename $0): image stopped $hours hours ago" >>~tinderbox/img/$oldimg/var/tmp/tb/EOL
+      elif __is_running $oldimg; then
+        echo -e "$(basename $0): last log $hours hours ago" >>~tinderbox/img/$oldimg/var/tmp/tb/EOL
+        sudo $(dirname $0)/kill_img.sh $oldimg
+      fi
+    fi
+  fi
+done < <(ImagesInRun)
+
+# free the slot in ~/run
+while read -r oldimg; do
+  if __is_stopped $oldimg; then
+    rm ~tinderbox/run/$oldimg
+    mv ~tinderbox/logs/$oldimg.log ~tinderbox/img/$oldimg/var/tmp/tb
+  fi
+done < <(
+  set +f
+  cd ~tinderbox/run
+  ls */var/tmp/tb/EOL 2>/dev/null | cut -f 1 -d '/' -s
+)
+
+# remove the chain around the cleanup
+rm $lockfile
+
+if FreeSlotAvailable; then
+  echo
+  date
+  echo " call setup"
+  tmpfile=$(mktemp /tmp/$(basename $0)_XXXXXX.$$.tmp)
+  set +e
+  # shellcheck disable=SC2024
+  sudo $(dirname $0)/setup_img.sh -s &>>$tmpfile
+  rc=$?
+  set -e
+  date
+  img=$(grep -m 1 -Eo '  name: .*' $tmpfile | awk '{ print $2 }')
+  if [[ $rc -eq 0 ]]; then
+    grep -A 10 -B 1 '^ OK' $tmpfile
+    mv $tmpfile ~tinderbox/img/$img/var/tmp/tb/$(basename $0).log
+  else
+    echo "setup failed  $img  rc: $rc  tmpfile: $tmpfile"
+  fi
 else
-  case $(nproc) in
-  32) desired_count=12 ;;
-  96) desired_count=16 ;;
-  *) desired_count=$(($(nproc) / 3)) ;;
-  esac
+  rc=0
 fi
 
-while :; do
-  while read -r oldimg; do
-    if [[ ! -f ~tinderbox/run/$oldimg/var/tmp/tb/EOL ]]; then
-      if [[ -f ~tinderbox/img/$oldimg/var/tmp/tb/task.log ]]; then
-        hours=$(((EPOCHSECONDS - $(stat -c %Z ~tinderbox/img/$oldimg/var/tmp/tb/task.log)) / 3600))
-      elif [[ -f ~tinderbox/img/$oldimg/var/tmp/tb/task ]]; then
-        hours=$(((EPOCHSECONDS - $(stat -c %Z ~tinderbox/img/$oldimg/var/tmp/tb/task)) / 3600))
-      else
-        hours=$(((EPOCHSECONDS - $(getStartTime $oldimg)) / 3600))
-      fi
-
-      if [[ $hours -ge 24 ]]; then
-        if __is_crashed $oldimg; then
-          echo -e "$(basename $0): image crashed $hours hours ago" >>~tinderbox/img/$oldimg/var/tmp/tb/EOL
-        elif __is_stopped $oldimg; then
-          echo -e "$(basename $0): image stopped $hours hours ago" >>~tinderbox/img/$oldimg/var/tmp/tb/EOL
-        elif __is_running $oldimg; then
-          echo -e "$(basename $0): last log $hours hours ago" >>~tinderbox/img/$oldimg/var/tmp/tb/EOL
-          sudo $(dirname $0)/kill_img.sh $oldimg
-        fi
-      fi
-    fi
-  done < <(ImagesInRunShuffled)
-
-  # free the slot
-  while read -r oldimg; do
-    if __is_stopped $oldimg; then
-      rm ~tinderbox/run/$oldimg
-      mv ~tinderbox/logs/$oldimg.log ~tinderbox/img/$oldimg/var/tmp/tb
-    fi
-  done < <(ImagesInRunButEOL)
-
-  if FreeSlotAvailable; then
-    echo
-    date
-    echo " call setup"
-    tmpfile=$(mktemp /tmp/$(basename $0)_XXXXXX.$$.tmp) # will be intentionally not removed in case of an issue
-    # shellcheck disable=SC2024
-    if sudo $(dirname $0)/setup_img.sh -s &>$tmpfile; then
-      date
-      img=$(grep -m 1 -Eo '  name: .*' $tmpfile | awk '{ print $2 }')
-      grep -A 10 -B 1 '^ OK' $tmpfile
-      mv $tmpfile ~tinderbox/img/$img/var/tmp/tb/$(basename $0).log
-    else
-      rc=$?
-      date
-      img=$(grep -m 1 -Eo '  name: .*' $tmpfile | awk '{ print $2 }')
-      echo "setup failed  $img  rc: $rc  tmpfile: $tmpfile"
-      exit $rc
-    fi
-    continue
-  fi
-
-  while read -r oldimg; do
-    if ! __is_running $oldimg; then
-      continue 2
-    fi
-    sleep 60
-  done < <(ImagesInRunButEOL)
-
-  # reaching this line means that there's nothing more to do
-  break
-done
-
-Finish
+exit $rc
