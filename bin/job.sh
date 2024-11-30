9179dc02bedfed3504e16da592e16454063026b6
Toralf Förster <toralf.foerster@gmx.de>
2024-11-30 19:23:00 +0100

    spelling

--- bin/job.sh
+++ bin/job.sh
@@ -497,7 +497,7 @@ function add2backlog() {
     echo "%emerge --resume" >>$bl
   else
     sed -i -e "/^$(sed -e 's,/,\\/,g' <<<$1)$/d" $bl # dups
-    echo "$1" >>$bl # append it == will be the next task
+    echo "$1" >>$bl                                  # append it == will be the next task
   fi
 }
 
