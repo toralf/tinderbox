143d5266d75e940540eb4fc080b900b110a6f15a
Toralf Förster <toralf.foerster@gmx.de>
2024-12-11 18:24:17 +0100

    fix options for @world

--- bin/job.sh
+++ bin/job.sh
@@ -740,7 +740,7 @@ function PostEmerge() {
   if grep -q ">>> Installing .* sys-devel/gcc-[1-9]" $tasklog_stripped; then
     if ! grep -q "%emerge -e @world" /var/tmp/tb/backlog.1st; then
       # needed at least for -flto and/or merged-usr to avoid segfaults of lto1
-      add2backlog "%emerge -1 /lib*/*.a /usr/lib*/*.a @world" # sometimes there're dep issues if @world is not included here
+      add2backlog "%emerge -1 /lib*/*.a /usr/lib*/*.a @world --update --changed-use --backtrack=50" # @world needed to avoid dep issues
     fi
     add2backlog "%SwitchGCC"
   fi
