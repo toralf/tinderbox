6997843394317eb00eb26f85e6effe891393d765
Toralf Förster <toralf.foerster@gmx.de>
2025-04-28 22:37:10 +0200

    do not search for (too) generic string "QA Notice"

--- bin/lib.sh
+++ bin/lib.sh
@@ -91,7 +91,7 @@ function prepareResultFile() {
 }
 
 function getSearchString() {
-  sed -e 's,^.* - ,,' -e 's,[\(\)], ,g'
+  sed -e 's,^.* - ,,' -e 's,[\(\)], ,g' -e 's,QA Notice: ,,'
 }
 
 # look for a blocker bug id
