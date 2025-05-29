3b7710d9ff8e7546f31442e7ba88261952a38cb9
Toralf Förster <toralf.foerster@gmx.de>
2025-05-29 12:27:21 +0200

    remove wrongly spelled matrics

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -2,31 +2,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-# use node_exporter's "textfile" feature to send metrics to Prometheus
-
 function printMetrics() {
-  # tinderbox_emerge_completed_img counter metrics should have "_total" suffix
-  local var="tinderbox_emerge_completed_img"
-  echo -e "# HELP $var Total number of completed emerges per image in ~/run\n# TYPE $var counter"
-  while read -r img; do
-    if c=$(grep -cF '::: completed emerge' ~tinderbox/run/$img/var/log/emerge.log) 2>/dev/null; then
-      echo "$var{img=\"$img\"} $c"
-    fi
-  done < <(find ~tinderbox/run/ -type l -print0 | xargs -r -n 1 --null basename)
-
   local var="tinderbox_emerge_completed_img_total"
   echo -e "# HELP $var Total number of completed emerges per image in ~/run\n# TYPE $var counter"
   while read -r img; do
     if c=$(grep -cF '::: completed emerge' ~tinderbox/run/$img/var/log/emerge.log) 2>/dev/null; then
-      echo "$var{img=\"$img\"} $c"
-    fi
-  done < <(find ~tinderbox/run/ -type l -print0 | xargs -r -n 1 --null basename)
-
-  # tinderbox_age_img counter metrics should have "_total" suffix
-  local var="tinderbox_age_img"
-  echo -e "# HELP $var Age of an image in ~/run\n# TYPE $var counter"
-  while read -r img; do
-    if c=$((EPOCHSECONDS - $(getStartTime $img))) 2>/dev/null; then
       echo "$var{img=\"$img\"} $c"
     fi
   done < <(find ~tinderbox/run/ -type l -print0 | xargs -r -n 1 --null basename)
