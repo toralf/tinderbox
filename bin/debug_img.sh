fe17ac933f0f801e0c369fac85b6381881df2d17
Toralf Förster <toralf.foerster@gmx.de>
2025-07-21 19:34:57 +0200

    make ps faux more convenient

--- bin/debug_img.sh
+++ bin/debug_img.sh
@@ -4,21 +4,23 @@
 
 set -euf
 export LANG=C.utf8
-export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+export PATH='./usr/sbin:./usr/bin:./sbin:./bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
 img=$(basename $1)
 child=$(jq -r '."child-pid"' /tmp/$img.json)
 
 cd ~tinderbox/img/$img
 
-echo " enter:    ps faux; exit"
-nsenter -t $child -F -C -i -p -u -r bash
-
-echo
-echo -n "pid (1): "
-read -r pid
-echo
-
-echo "get gdb trace for $img with child-pid $child for pid $pid"
 nsenter -t $child -F -a -r \
-  gdb -q -batch -ex "set logging enabled off" -ex "bt full" -ex "detach" -ex "quit" -p ${pid:-1}
+  bash -c 'ps faux; exit'
+
+echo
+pid=1
+echo -n "pid ($pid): "
+read -r input
+[[ -n $input ]] && pid=$input
+echo
+
+echo "get gdb bt for $img with child-pid $child for pid $pid"
+nsenter -t $child -F -a -r \
+  gdb -q -batch -ex 'set logging enabled off' -ex 'thread apply all bt' -ex 'detach' -ex 'quit' -p $pid
