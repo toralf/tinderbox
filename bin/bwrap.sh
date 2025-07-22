523bd0d099ba1870f2678f52318d4a8e3618a4ed
Toralf Förster <toralf.foerster@gmx.de>
2025-07-22 12:06:47 +0200

    give 4 GiB per compile job instead of 3

--- bin/bwrap.sh
+++ bin/bwrap.sh
@@ -12,11 +12,11 @@ function CreateCgroup() {
     if mkdir $cgdomain 2>/dev/null; then
       echo "+cpu +cpuset +memory" >$cgdomain/cgroup.subtree_control
 
-      # reserve 5 of 32 vCPU for non-tinderboxing work
+      # reserve 5 of 32 vCPU for non-tinderboxing tasks (e.g. AFL++ for openssl and Tor)
       echo "$((27 * 100))" >$cgdomain/cpu.weight
       echo "$((27 * 100000))" >$cgdomain/cpu.max
-      echo "110G" >$cgdomain/memory.max      # images x (jobs x RAM + 1) GiB
-      echo "200G" >$cgdomain/memory.swap.max # 256 GiB swap currently
+      echo "110G" >$cgdomain/memory.max
+      echo "200G" >$cgdomain/memory.swap.max
     fi
   fi
 
@@ -46,8 +46,8 @@ function CreateCgroup() {
   echo "$((100 * jobs))" >$name/cpu.weight
   echo "$((100000 * jobs))" >$name/cpu.max
 
-  # 3 GiB per job + 1 GiB for misc
-  echo "$((3 * jobs + 1))G" >$name/memory.max
+  # N GiB per job + 1 GiB on top
+  echo "$((4 * jobs + 1))G" >$name/memory.max
 }
 
 function RemoveCgroup() {
