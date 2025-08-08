622502426d487d65fb669ad7280c7bc109f246f7
Toralf Förster <toralf.foerster@gmx.de>
2025-08-08 20:24:54 +0200

    fix locale

--- bin/setup_img.sh
+++ bin/setup_img.sh
@@ -605,7 +605,6 @@ if [[ ! $profile =~ "/musl" ]]; then
   date
   echo "#setup locale" | tee /var/tmp/tb/task
   cat <<EOF2 >>/etc/locale.gen
-C.UTF8 UTF-8
 en_US ISO-8859-1
 # needed by Dotnet SDK and for +test
 en_US.UTF-8 UTF-8
